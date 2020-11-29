edc06b9f3c6b35e23fbd345e2c5ccc66281a989c
Nick Cao <nickcao@nichi.co>
2020-11-29 13:11:47 +0800

    refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -2,28 +2,22 @@
   description = "a nix derivation collection by nickcao";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let
+      genPkgs = val:
+        with builtins;
+        listToAttrs (map (name: {
+          inherit name;
+          value = val name;
+        }) (attrNames (readDir ./pkgs)));
+    in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
           overlays = [ self.overlay ];
         };
-      in {
-        packages = {
-          auth-thu = pkgs.auth-thu;
-          qv2ray = pkgs.qv2ray;
-          v2ray-core = pkgs.v2ray-core;
-          rait = pkgs.rait;
-          smartdns-china-list = pkgs.smartdns-china-list;
-        };
-      }) // {
-        overlay = final: prev: {
-          auth-thu = final.callPackage ./pkgs/auth-thu { };
-          qv2ray = final.callPackage ./pkgs/qv2ray { };
-          v2ray-core = final.callPackage ./pkgs/v2ray-core { };
-          rait = final.callPackage ./pkgs/rait { };
-          smartdns-china-list = final.callPackage ./pkgs/smartdns-china-list { };
-        };
+      in { packages = genPkgs (name: pkgs.${name}); }) // {
+        overlay = final: prev:
+          genPkgs (name: final.callPackage (./pkgs + "/${name}") { });
       };
 }
