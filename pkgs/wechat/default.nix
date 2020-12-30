10a2a79d56871ce202e0ce63a397adc3606d622b
Nick Cao <nickcao@nichi.co>
2020-12-30 19:57:02 +0800

    updated wechat

--- pkgs/wechat/default.nix
+++ pkgs/wechat/default.nix
@@ -1,5 +1,28 @@
-{ stdenv, writeShellScriptBin, writeText, fetchurl, steam-run-native, dpkg }:
+{ stdenv, writeShellScriptBin, writeText, fetchurl, steam, dpkg, bubblewrap }:
 let
+  wechat-run = (steam.override {
+    nativeOnly = true;
+    extraPkgs = pkgs: [
+      (stdenv.mkDerivation rec {
+        pname = "wechat-wrapped";
+        version = "2.0.0";
+
+        src = fetchurl {
+          url = "https://cdn-package-store6.deepin.com/appstore/pool/appstore/c/com.qq.weixin/com.qq.weixin_${version}_amd64.deb";
+          sha256 = "0l5i3nvl241y4cwwv733b170bjqfipybm7ky277bhkm49jph808b";
+        };
+
+        phases = [ "installPhase" ];
+
+        installPhase = ''
+          ${dpkg}/bin/dpkg -x $src .
+          mv usr/ $out/
+          mv opt/apps/com.qq.weixin/ $out/share/
+        '';
+      })
+    ];
+  }).run;
+
   lsb-release = writeText "lsb-release" ''
     DISTRIB_ID=uos
     DISTRIB_RELEASE=20
@@ -16,26 +39,9 @@ let
     BUG_REPORT_URL=http://bbs.chinauos.com
     VERSION_CODENAME=eagle
   '';
-  wechat-wrapped = stdenv.mkDerivation rec {
-    pname = "wechat-wrapped";
-    version = "2.0.0";
-
-    src = fetchurl {
-      url = "https://cdn-package-store6.deepin.com/appstore/pool/appstore/c/com.qq.weixin/com.qq.weixin_${version}_amd64.deb";
-      sha256 = "0l5i3nvl241y4cwwv733b170bjqfipybm7ky277bhkm49jph808b";
-    };
-
-    phases = [ "installPhase" ];
-
-    installPhase = ''
-      ${dpkg}/bin/dpkg -x $src $out
-      sed -i 's/\/etc\/lsb-release/\/tmp\/lsb-release/' $out/opt/apps/com.qq.weixin/files/wechat
-      sed -i 's/\/etc\/os-release/\/tmp\/os-release/' $out/usr/lib/license/libuosdevicea.so
-    '';
-  };
 in
 writeShellScriptBin "wechat" ''
-  cp ${lsb-release} /tmp/lsb-release
-  cp ${os-release} /tmp/os-release
-  ${steam-run-native}/bin/steam-run ${wechat-wrapped}/opt/apps/com.qq.weixin/files/wechat
+  ${wechat-run}/bin/steam-run ${bubblewrap}/bin/bwrap --dev-bind / / \
+    --symlink ${os-release} /etc/os-release --symlink ${lsb-release} /etc/lsb-release \
+    /usr/share/com.qq.weixin/files/wechat
 ''
