1bca9d1ae6d3eaf44321cb6347e2bb7107c68e32
Nick Cao <nickcao@nichi.co>
2020-12-30 22:10:39 +0800

    added openredir

---
+++ pkgs/openredir/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchFromGitHub, automake, autoconf, libtool, lua }:
+stdenv.mkDerivation rec {
+  pname = "openredir";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "lilydjwg";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-aaLbauopSRshV5gvExwkA+9gTzq/Jm7lu4wNHLnGzRI=";
+  };
+
+  buildInputs = [ autoconf automake libtool lua ];
+  buildPhase = ''
+    ./autogen.sh
+    ./configure --prefix=$out
+    make
+  '';
+}
