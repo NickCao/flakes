060b7ad5ac74186575985348e465d909599d8339
Nick Cao <nickcao@nichi.co>
2021-01-23 21:10:35 +0800

    remove gpg agent hack

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -162,9 +162,6 @@ in
         # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
         ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
         ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-
-        # hack for gpg agent issue
-        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0406", RUN+="${pkgs.sudo}/bin/sudo -u nickcao ${pkgs.gnupg}/bin/gpg --card-status"
       '';
     };
     xserver = {
