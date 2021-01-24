8e5e1ee4fb47e9f34fe1eaf7d42f9ec5793122b5
Nick Cao <nickcao@nichi.co>
2021-01-24 16:47:13 +0800

    remap caps to bksp with hwdb instead of xkb

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -165,13 +165,16 @@ in
         ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
         ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
       '';
+      extraHwdb = ''
+        evdev:input:b0005v05ACp024F*
+          KEYBOARD_KEY_70039=backspace
+      '';
     };
     xserver = {
       enable = true;
       displayManager = { gdm = { enable = true; }; };
       desktopManager = { gnome3 = { enable = true; }; };
       videoDrivers = [ "nvidia" ];
-      xkbOptions = "caps:backspace";
     };
     pcscd = { enable = true; };
     smartdns = {
