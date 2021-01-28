57a8c135f9a57946663254279bcd8e2d6dc00bdc
Nick Cao <nickcao@nichi.co>
2021-01-28 19:32:18 +0800

    moved alacritty themes to seperate file

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,65 +1,6 @@
 { pkgs, config, ... }:
 let
-  dark = {
-    bright = {
-      black = "#002b36";
-      blue = "#839496";
-      cyan = "#93a1a1";
-      green = "#586e75";
-      magenta = "#6c71c4";
-      red = "#cb4b16";
-      white = "#fdf6e3";
-      yellow = "#657b83";
-    };
-    cursor = {
-      cursor = "#839496";
-      text = "#002b36";
-    };
-    normal = {
-      black = "#073642";
-      blue = "#268bd2";
-      cyan = "#2aa198";
-      green = "#859900";
-      magenta = "#d33682";
-      red = "#dc322f";
-      white = "#eee8d5";
-      yellow = "#b58900";
-    };
-    primary = {
-      background = "#002b36";
-      foreground = "#839496";
-    };
-  };
-  light = {
-    bright = {
-      black = "#002b36";
-      blue = "#839496";
-      cyan = "#93a1a1";
-      green = "#586e75";
-      magenta = "#6c71c4";
-      red = "#cb4b16";
-      white = "#fdf6e3";
-      yellow = "#657b83";
-    };
-    cursor = {
-      cursor = "#657b83";
-      text = "#fdf6e3";
-    };
-    normal = {
-      black = "#073642";
-      blue = "#268bd2";
-      cyan = "#2aa198";
-      green = "#859900";
-      magenta = "#d33682";
-      red = "#dc322f";
-      white = "#eee8d5";
-      yellow = "#b58900";
-    };
-    primary = {
-      background = "#fdf6e3";
-      foreground = "#657b83";
-    };
-  };
+  toTOMLDrv = v: (pkgs.formats.toml { }).generate "" v;
 in
 {
   home.packages = with pkgs; [ sops update-nix-fetchgit drone-cli buildifier ];
@@ -135,7 +76,7 @@ in
           normal = { family = "JetBrains Mono"; };
           size = 13;
         };
-        colors = light;
+        colors = (import ./alacritty.nix).light;
         shell = {
           program = "${pkgs.tmux}/bin/tmux";
           args = [ "attach" ];
@@ -184,11 +125,12 @@ in
         '';
       };
       "containers/storage.conf" = {
-        text = ''
-          [storage]
-          driver = "btrfs"
-          rootless_storage_path = "$HOME/Data/Containers/"
-        '';
+        source = toTOMLDrv {
+          storage = {
+            driver = "btrfs";
+            rootless_storage_path = "$HOME/Data/Containers/";
+          };
+        };
       };
     };
   };
