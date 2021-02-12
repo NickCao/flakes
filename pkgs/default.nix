76bfdc8249a0c722149407943f40be09c5f67d38
Nick Cao <nickcao@nichi.co>
2021-02-12 12:18:43 +0800

    removed vscode extension terraform

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 rec {
   mapPackages = f: with builtins;listToAttrs (map (name: { inherit name; value = f name; }) (filter (v: v != null) (attrValues (mapAttrs (k: v: if v == "directory" then k else null) (readDir ./.)))));
-  getPackages = pkgs: pkgs.lib.filterAttrs (n: p: p.meta.only or true) (mapPackages (name: pkgs.${name}));
+  getPackages = pkgs: mapPackages (name: pkgs.${name});
   overlay = final: prev: mapPackages (name: final.callPackage (./. + "/${name}") { });
 }
