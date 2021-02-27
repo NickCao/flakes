e42269e0370643b2099c727b3f940b2befae183d
Nick Cao <nickcao@nichi.co>
2021-02-27 11:53:50 +0800

    added package mode

---
+++ pkgs/mode/default.nix
@@ -0,0 +1,23 @@
+{ writeShellScriptBin, xxd, upower, gnugrep, systemd }:
+writeShellScriptBin "mode" ''
+  ECREG=`sudo ${xxd}/bin/xxd -s 0x1d -l 1 -ps /sys/kernel/debug/ec/ec0/io`
+  echo -n "Fan Control: "
+  case $ECREG in
+    00)
+      echo "Balanced Mode";;
+    01)
+      echo "Beast Mode";;
+    02)
+      echo "Quiet Mode";;
+  esac
+
+  echo -n "Freq Governor: "
+  cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+  echo -n "GPU Status: "
+  cat /sys/bus/pci/devices/0000:01:00.0/power/runtime_status
+
+  echo "Battery:"
+  ${upower}/bin/upower -i /org/freedesktop/UPower/devices/battery_BAT0 | ${gnugrep}/bin/grep -E 'state|percentage|time to empty|energy-rate'
+
+  ${systemd}/lib/systemd/systemd-boot-check-no-failures
+''
