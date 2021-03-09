ef6da8f36c2e9df8e92268bd39fccf26baf4fdc6
Nick Cao <nickcao@nichi.co>
2021-03-09 21:32:48 +0800

    reworked v2ray-geoip packaging

--- pkgs/v2ray-geoip/default.nix
+++ pkgs/v2ray-geoip/default.nix
@@ -1,17 +1,14 @@
-{ fetchurl, stdenv, lib }:
+{ fetchFromGitHub, stdenv, lib }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "v2ray-geoip";
-  version = "202012240026";
-  srcs = fetchurl {
-    url =
-      "https://github.com/v2fly/geoip/releases/download/${version}/geoip.dat";
-    sha256 = "sha256-T8yCJZ5t1C20b6FCiFrILzFIsJkpWcH3FvOTgvejk60=";
+  version = "2021-03-08";
+  src = fetchFromGitHub {
+    owner = "v2fly";
+    repo = "geoip";
+    rev = "7fc41f5cd05926f39da95c53aba2a85280c0ad5f"; # heads/release
+    sha256 = "1iwxyl6a8lnbwcgm8m3ypxd3bq1lg4c58dqjn4956jib13w2f2nw";
   };
-
-  unpackCmd = ''
-    install -m 0644 $curSrc -D src/$(stripHash $curSrc)
-  '';
 
   installPhase = ''
     install -m 0644 geoip.dat -D $out/share/v2ray/geoip.dat
