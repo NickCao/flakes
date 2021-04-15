4ad949cc9383e5df0e0f6499adf912ee41d341f1
Nick Cao <nickcao@nichi.co>
2021-04-15 16:08:18 +0800

    temporary workaround for broken gpg-agent

--- nixos/home.nix
+++ nixos/home.nix
@@ -35,6 +35,7 @@ in
   ];
 
   systemd.user.sessionVariables = {
+    GNUPGHOME = "${config.xdg.dataHome}/gnupg";
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
     LIBVA_DRIVER_NAME = "iHD";
