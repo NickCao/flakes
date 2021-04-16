f4902c4178b69654a878e23d09707a2bf3a3a80b
Nick Cao <nickcao@nichi.co>
2021-04-16 21:06:05 +0800

    didn't find a better way to override a single package in package set

--- nixos/default.nix
+++ nixos/default.nix
@@ -8,7 +8,22 @@ nixpkgs.lib.nixosSystem {
     home-manager.nixosModules.home-manager
     sops-nix.nixosModules.sops
     {
-      nixpkgs.overlays = [ self.overlay neovim.overlay fenix.overlay ];
+      nixpkgs.overlays = [
+        (
+          final: prev: {
+            gnome3 = prev.gnome3 // {
+              gnome-shell = prev.gnome3.gnome-shell.overrideAttrs (attrs: {
+                pname = "gnome-shell-fixed";
+                buildInputs = attrs.buildInputs ++ [ prev.mesa ];
+              });
+              gnome-session = prev.gnome3.gnome-session.override { gnome3 = final.gnome3; };
+            };
+          }
+        )
+        self.overlay
+        neovim.overlay
+        fenix.overlay
+      ];
       nix.registry.p.flake = nixpkgs;
       home-manager.users.nickcao.imports = [ "${impermanence}/home-manager.nix" ];
     }
