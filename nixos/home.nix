94344ae3e7cb130373beca459dd004c5801e7f7b
Nick Cao <nickcao@nichi.co>
2021-04-19 09:52:32 +0800

    bump

--- nixos/home.nix
+++ nixos/home.nix
@@ -33,7 +33,6 @@ in
   ];
 
   systemd.user.sessionVariables = {
-    GNUPGHOME = "${config.xdg.dataHome}/gnupg";
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
     LIBVA_DRIVER_NAME = "iHD";
