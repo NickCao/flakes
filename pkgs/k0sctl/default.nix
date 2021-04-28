d8100f6e2f216c17131d57f9c5180d37237188f2
Nick Cao <nickcao@nichi.co>
2021-04-29 07:58:26 +0800

    fix vendor sha256 of k0sctl

--- pkgs/k0sctl/default.nix
+++ pkgs/k0sctl/default.nix
@@ -1,7 +1,7 @@
 { source, buildGoModule, fetchFromGitHub, lib }:
 buildGoModule rec {
   inherit (source) pname version src;
-  vendorSha256 = "sha256-Cv8Huor4WdlPvfZkuyim1plM+afqbbig9F8oRQNyfRo=";
+  vendorSha256 = "sha256-7fT1FZXiSPyEvlOwXOQlpR6g+izTnTRDK9A+CnVWOTA=";
   subPackages = [ "." ];
   meta = with lib; {
     homepage = "https://github.com/k0sproject/k0sctl";
