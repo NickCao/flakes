d10c6bef0e5e3b9f7a8dc16a30f9013db2ba044a
Nick Cao <nickcao@nichi.co>
2021-05-08 10:41:22 +0800

    track nixos-unstable-small instead of gnome-40

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "a nix derivation collection by nickcao";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/gnome-40";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     impermanence.url = "github:nix-community/impermanence";
     rust-overlay = {
