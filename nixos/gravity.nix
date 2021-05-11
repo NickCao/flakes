e48e31dc8eeb347a62a6eb619f51e717ff68cb6a
Nick Cao <nickcao@nichi.co>
2021-05-11 21:43:37 +0800

    never gonna give vrf a second chance

--- nixos/gravity.nix
+++ nixos/gravity.nix
@@ -23,7 +23,6 @@
           export where proto = "inject";
         };
         randomize router id;
-        vrf "vrf-gravity";
         interface "gravity";
       }
       protocol kernel {
@@ -31,8 +30,6 @@
           import none;
           export all;
         };
-        vrf "vrf-gravity";
-        kernel table 100;
       }
     '';
   };
@@ -42,10 +39,8 @@
       serviceConfig = with pkgs;{
         ExecStartPre = [
           "-${iproute}/bin/ip netns add gravity"
-          "-${iproute}/bin/ip link add vrf-gravity type vrf table 100"
-          "${iproute}/bin/ip link set vrf-gravity up"
           "-${iproute}/bin/ip link add gravity type veth peer host netns gravity"
-          "${iproute}/bin/ip link set gravity up master vrf-gravity"
+          "${iproute}/bin/ip link set gravity up"
           "${iproute}/bin/ip addr replace 2a0c:b641:69c:99cc::2/128 dev gravity"
           "${iproute}/bin/ip -n gravity link set host up"
           "${iproute}/bin/ip -n gravity link set up lo"
@@ -57,7 +52,6 @@
         ExecStopPost = [
           "${rait}/bin/rait down"
           "${iproute}/bin/ip link del gravity"
-          "${iproute}/bin/ip link del vrf-gravity"
           "${iproute}/bin/ip netns del gravity"
         ];
       };
