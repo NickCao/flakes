be82c902cfa509c603e021da11b8f79b9ab5fc4e
Nick Cao <nickcao@nichi.co>
2021-05-22 00:23:23 +0800

    hardened tls options

--- nixos/nrt/services.nix
+++ nixos/nrt/services.nix
@@ -51,6 +51,15 @@
       };
     };
     dynamicConfigOptions = {
+      tls.options.default = {
+        minVersion = "VersionTLS12";
+        sniStrict = true;
+        cipherSuites = [
+          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+        ];
+      };
       http = {
         routers = {
           blog = {
