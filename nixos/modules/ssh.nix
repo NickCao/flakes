d7d464a5fad8b97fd8b1b0407e6dac08fcbcf81f
Nick Cao <nickcao@nichi.co>
2021-06-08 19:23:01 +0800

    added ssh pubkey for ci

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -3,6 +3,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNPLArhyazrFjK4Jt/ImHSzICvwKOk4f+7OEcv2HEb7"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpzrZLU0peDu1otGtP2GcCeQIkI8kmfHjnwpbfpWBkv"
   ];
   services.openssh.enable = true;
 }
