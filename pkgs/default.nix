c5806fec0e9bdd4817831a2d871811ad38b5b63a
Nick Cao <nickcao@nichi.co>
2021-06-17 15:25:59 +0800

    fix pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 rec {
-  mapPackages = f: with builtins;listToAttrs (map (name: { inherit name; value = f name; }) (filter (v: v != null) (attrValues (mapAttrs (k: v: if v == "directory" && k != "_build" then k else null) (readDir ./.)))));
+  mapPackages = f: with builtins;listToAttrs (map (name: { inherit name; value = f name; }) (filter (v: v != null) (attrValues (mapAttrs (k: v: if v == "directory" && k != "_sources" then k else null) (readDir ./.)))));
   packages = pkgs: mapPackages (name: pkgs.${name});
   overlay = final: prev: mapPackages (name:
     let
