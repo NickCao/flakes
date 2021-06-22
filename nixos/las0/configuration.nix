48740cc83205af0eddd9d58d302cf7bcb9c15e96
Nick Cao <nickcao@nichi.co>
2021-06-22 21:27:54 +0800

    configure ipv6 address on las0

--- nixos/las0/configuration.nix
+++ nixos/las0/configuration.nix
@@ -31,4 +31,19 @@ in
       expand-alias=yes
     '';
   };
+
+  systemd.network.networks = {
+    ens3 = {
+      name = "ens3";
+      address = [ "2605:6400:20:387::1/48" ];
+      routes = [
+        {
+          routeConfig = {
+            Gateway = "2605:6400:2:fed5::1";
+            GatewayOnLink = true;
+          };
+        }
+      ];
+    };
+  };
 }
