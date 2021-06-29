0ac58965f9e890df69277e878895fe46f7876165
Nick Cao <nickcao@nichi.co>
2021-06-29 17:08:20 +0800

    update zonefile

--- nixos/modules/dns/default.nix
+++ nixos/modules/dns/default.nix
@@ -5,11 +5,11 @@ let
   zones = {
     "db.9.6.0.1.4.6.b.c.0.a.2.ip6.arpa" = pkgs.fetchurl {
       url = "https://s3.nichi.co/artifacts/gravity/db.9.6.0.1.4.6.b.c.0.a.2.ip6.arpa";
-      sha256 = "sha256-S4ev2aZhvbceE+M9mdvmSuu16O61zuT1EpgWg0nIMog=";
+      sha256 = "sha256-Cb27SL3Te73I02bU6tFQ/UOWt7uH8p68fvBrx7sLQVc=";
     };
     "db.gravity" = pkgs.fetchurl {
       url = "https://s3.nichi.co/artifacts/gravity/db.gravity";
-      sha256 = "sha256-Dm0UMjEeIcDezdeRW2kIXqSGUwOPbNaWPsBOjJ3AqlY=";
+      sha256 = "sha256-ryqiDBjwILGHQsmSk9cr/ypW+dJyYKJp2QN0P5GXBac=";
     };
   };
 in
