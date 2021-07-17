ce1076ae0239728d99ed28a7a8c98b39d5c797b5
Nick Cao <nickcao@nichi.co>
2021-07-17 09:38:15 +0800

    replaced fwmark hacks with bird recursive routes

--- nixos/local/gravity.nix
+++ nixos/local/gravity.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 let
-  china6 = pkgs.runCommand "china6"
-    {
+  china6 = pkgs.runCommand "china6" {
       src = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/1535d66331c10102d56712d07531373d8769cf41/china6.txt";
         sha256 = "sha256-NOrG/KP0M17oE7K4bbCE8Q39w3oB8M1cbwBLzstljNY=";
       };
     } ''
-    echo "define china6 = {" > $out
-    sed '$!s/$/,/' $src >> $out
-    echo "}" >> $out
+    sed 's/^/route /;s/$/ recursive 2001:db8::1;/' $src >> $out
   '';
 in
 {
@@ -20,10 +17,10 @@ in
     address = "2a0c:b641:69c:99cc::1/126";
     postStart = [
       "${pkgs.iproute2}/bin/ip addr add 2a0c:b641:69c:99cc::2/126 dev gravity"
-      "-${pkgs.iproute2}/bin/ip -6 ru add fwmark 0x36 lookup main pref 1024"
-      "-${pkgs.iproute2}/bin/ip -6 ru add fwmark 0x36 blackhole pref 1025"
-      "-${pkgs.iproute2}/bin/ip -6 ru add fwmark 0x37 lookup main pref 1026"
-      "-${pkgs.iproute2}/bin/ip -6 ru add lookup 100 pref 1027"
+      "-${pkgs.iproute2}/bin/ip -6 ru add fwmark 0x36 lookup main pref 1022"
+      "-${pkgs.iproute2}/bin/ip -6 ru add fwmark 0x36 blackhole pref 1023"
+      "-${pkgs.iproute2}/bin/ip -6 ru add lookup 101 pref 1024"
+      "-${pkgs.iproute2}/bin/ip -6 ru add lookup 100 pref 1025"
     ];
   };
   services.bird2 = {
@@ -31,6 +28,7 @@ in
     config = ''
       router id 169.254.0.1;
       ipv6 sadr table sadr6;
+      ipv6 table china6;
       protocol device { }
       protocol static inject {
         ipv6 sadr;
@@ -44,32 +42,35 @@ in
         randomize router id;
         interface "gravity";
       }
-      protocol kernel {
+      protocol static china6_static {
+        ipv6 {
+          table china6;
+        };
+        igp table master6;
+        include "${china6}";
+      }
+      protocol kernel gravity_kernel {
         ipv6 sadr {
           import none;
           export all;
         };
         kernel table 100;
       }
+      protocol kernel china6_kernel {
+        ipv6 {
+          table china6;
+          import none;
+          export all;
+        };
+        kernel table 101;
+      }
+      protocol kernel master6_kernel {
+        ipv6 {
+          import all;
+          export all;
+        };
+        learn;
+      }
     '';
   };
-  networking.nftables.enable = true;
-  networking.nftables.ruleset = ''
-    include "${china6}"
-    table ip6 route {
-        set china {
-            type ipv6_addr
-            flags interval
-            elements = $china6
-        }
-        chain china {
-            type route hook output priority 0;
-            ip6 daddr @china meta mark set 0x37;
-        }
-        chain nat {
-            type nat hook postrouting priority 0;
-            masquerade;
-        }
-    }
-  '';
 }
