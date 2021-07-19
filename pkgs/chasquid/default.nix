920e344b4db0bbcbe6ec2c89c063afc36fc794f5
Nick Cao <nickcao@nichi.co>
2021-07-19 17:23:20 +0800

    pkgs: add man pages for chasquid

--- pkgs/chasquid/default.nix
+++ pkgs/chasquid/default.nix
@@ -1,4 +1,4 @@
-{ source, buildGoModule, lib }:
+{ source, buildGoModule, lib, installShellFiles }:
 buildGoModule rec {
   inherit (source) pname version src;
   vendorSha256 = "sha256-1H1zTRzX6a4mBSHIJvLeVC9GIKE8qUvwbgfRw297vq4=";
@@ -6,6 +6,12 @@ buildGoModule rec {
     rm internal/courier/smtp_test.go
     rm internal/courier/mda_test.go
   '';
+  postInstall = ''
+    installManPage docs/man/*.1 docs/man/*.5
+  '';
+  excludedPackages = "\\(cmd/dovecot-auth-cli\\|cmd/spf-check\\)";
+  nativeBuildInputs = [ installShellFiles ];
+  outputs = [ "out" "man" ];
   meta = with lib; {
     description = "an SMTP (email) server with a focus on simplicity, security, and ease of operation.";
     homepage = "https://blitiri.com.ar/p/chasquid/";
