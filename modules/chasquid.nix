70d1fcf93af35c95dc1cc94d46bb338e9536a3af
Nick Cao <nickcao@nichi.co>
2021-07-20 13:44:19 +0800

    modules: updated chasquid

--- modules/chasquid.nix
+++ modules/chasquid.nix
@@ -2,7 +2,43 @@
 with lib;
 let
   cfg = config.services.chasquid;
-  domainType = types.submodule ({ config, ... }: {});
+  configFile = optionalString (cfg.hostname != null) ''
+    hostname: "${cfg.hostname}"
+  '' + ''
+    max_data_size_mb: ${toString cfg.maxDataSize}
+  '' + concatMapStrings (addr: "smtp_address: \"${addr}\"\n") cfg.smtpAddresses
+  + concatMapStrings (addr: "submission_address: \"${addr}\"\n") cfg.submissionAddresses
+  + concatMapStrings (addr: "submission_over_tls_address: \"${addr}\"\n") cfg.submissionTLSAddresses
+  + optionalString (cfg.monitoringAddress != null) ''
+    monitoring_address: "${cfg.monitoringAddress}"
+  '' + ''
+    mail_delivery_agent_bin: "${cfg.mdaBin}"
+  '' + concatMapStrings (arg: "mail_delivery_agent_args: \"${arg}\"\n") cfg.mdaArgs + ''
+    data_dir: "${cfg.dataDir}"
+    suffix_separators: "${cfg.suffixSeparators}"
+    drop_characters: "${cfg.dropCharacters}"
+    mail_log_path: "<stderr>"
+    dovecot_auth: ${boolToString cfg.dovecotAuth}
+    haproxy_incoming: ${boolToString cfg.proxyIncoming}
+  '' + optionalString (cfg.dovecotUserdb != null) ''
+    dovecot_userdb_path: "${cfg.dovecotUserdb}"
+  '' + optionalString (cfg.dovecotClient != null) ''
+    dovecot_client_path: "${cfg.dovecotClient}"
+  '';
+  domainType = types.submodule ({ config, ... }: {
+    options = {
+      aliases = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        description = "aliases for the domain.";
+      };
+      users = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        description = "users for the domain.";
+      };
+    };
+  });
 in
 {
   options.services.chasquid = {
@@ -89,28 +125,6 @@ in
     };
   };
   config = mkIf cfg.enable {
-    environment.etc."chasquid/chasquid.conf".text = optionalString (cfg.hostname != null) ''
-      hostname: "${cfg.hostname}"
-    '' + ''
-      max_data_size_mb: ${toString cfg.maxDataSize}
-    '' + concatMapStrings (addr: "smtp_address: \"${addr}\"\n") cfg.smtpAddresses
-    + concatMapStrings (addr: "submission_address: \"${addr}\"\n") cfg.submissionAddresses
-    + concatMapStrings (addr: "submission_over_tls_address: \"${addr}\"\n") cfg.submissionTLSAddresses
-    + optionalString (cfg.monitoringAddress != null) ''
-      monitoring_address: "${cfg.monitoringAddress}"
-    '' + ''
-      mail_delivery_agent_bin: "${cfg.mdaBin}"
-    '' + concatMapStrings (arg: "mail_delivery_agent_args: \"${arg}\"\n") cfg.mdaArgs + ''
-      data_dir: "${cfg.dataDir}"
-      suffix_separators: "${cfg.suffixSeparators}"
-      drop_characters: "${cfg.dropCharacters}"
-      mail_log_path: "<stderr>"
-      dovecot_auth: ${boolToString cfg.dovecotAuth}
-      haproxy_incoming: ${boolToString cfg.proxyIncoming}
-    '' + optionalString (cfg.dovecotUserdb != null) ''
-      dovecot_userdb_path: "${cfg.dovecotUserdb}"
-    '' + optionalString (cfg.dovecotClient != null) ''
-      dovecot_client_path: "${cfg.dovecotClient}"
-    '';
+    environment.etc."chasquid/chasquid.conf".text = configFile;
   };
 }
