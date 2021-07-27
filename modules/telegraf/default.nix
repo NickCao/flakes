02f6751cc96ae6660e6848aa0665558f98631295
Nick Cao <nickcao@nichi.co>
2021-07-27 11:38:22 +0800

    reconfigure telegraf

--- modules/telegraf/default.nix
+++ modules/telegraf/default.nix
@@ -4,17 +4,30 @@ with lib;
   sops.secrets.telegraf = {
     sopsFile = ./secrets.yaml;
   };
-  systemd.services.telegraf = {
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network-online.target" ];
-    serviceConfig = {
-      EnvironmentFile = config.sops.secrets.telegraf.path;
-      DynamicUser = true;
-      ExecStart = "${pkgs.telegraf}/bin/telegraf -config https://stats.nichi.co/api/v2/telegrafs/07e44bec596d9000";
-      ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
-      Restart = "on-failure";
-      RestartForceExitStatus = "SIGPIPE";
-      KillMode = "control-group";
+  services.telegraf = {
+    enable = true;
+    environmentFiles = [ config.sops.secrets.telegraf.path ];
+    extraConfig = {
+      outputs = {
+        influxdb_v2 = {
+          urls = [ "https://stats.nichi.co" ];
+          token = "$INFLUX_TOKEN";
+          organization = "nichi";
+          bucket = "stats";
+        };
+      };
+      inputs = {
+        cpu = { };
+        disk = {
+          ignore_fs = [ "tmpfs" "devtmpfs" "devfs" "overlay" "aufs" "squashfs" ];
+        };
+        diskio = { };
+        mem = { };
+        net = { };
+        processes = { };
+        system = { };
+        systemd_units = { };
+      };
     };
   };
 }
