a4be998e8e25eff399b85a52b32018ca89f1e162
Nick Cao <nickcao@nichi.co>
2021-07-28 17:40:13 +0800

    first working version on the new image

--- modules/image.nix
+++ modules/image.nix
@@ -3,24 +3,65 @@ with pkgs;
 let
   toplevel = config.system.build.toplevel;
   db = closureInfo { rootPaths = [ toplevel ]; };
-  rootfs = config.fileSystems."/";
+  devPath = "/dev/disk/by-partlabel/NIXOS";
 in
 {
-  boot.loader.grub.device = lib.mkForce "/dev/sda";
-  system.build.image = runCommandNoCC "nixos.img" { } ''
-    export TERM=dumb
-    export HOME=$TMPDIR/home
-    export root=$TMPDIR/root
+  boot = {
+    postBootCommands = ''
+      echo "Fix" | ${parted}/bin/parted /dev/vda ---pretend-input-tty print
+      ${parted}/bin/parted --script /dev/vda resizepart 2 100%
+      ${btrfs-progs}/bin/btrfs fi resize max /nix
+    '';
+    tmpOnTmpfs = true;
+    loader.grub.device = "/dev/vda";
+  };
+
+  services.getty.autologinUser = "root";
+
+  fileSystems."/" = {
+    fsType = "tmpfs";
+    options = [ "defaults" "size=2G" "mode=755" ];
+  };
+
+  fileSystems."/boot" = {
+    device = devPath;
+    fsType = "btrfs";
+    options = [ "subvol=boot" "noatime" "compress-force=zstd" "space_cache=v2" ];
+  };
+
+  fileSystems."/nix" = {
+    device = devPath;
+    fsType = "btrfs";
+    options = [ "subvol=nix" "noatime" "compress-force=zstd" "space_cache=v2" ];
+  };
+
+  fileSystems."/persist" = {
+    device = devPath;
+    fsType = "btrfs";
+    options = [ "subvol=persist" "noatime" "compress-force=zstd" "space_cache=v2" ];
+  };
+
+  system.build.image = vmTools.runInLinuxVM (runCommand "image"
+    {
+      preVM = ''
+        mkdir $out
+        diskImage=$out/nixos.img
+        ${vmTools.qemu}/bin/qemu-img create -f raw $diskImage 2G
+      '';
+      nativeBuildInputs = [ parted btrfs-progs mount util-linux nixUnstable config.system.build.nixos-install ];
+    } ''
+    parted --script /dev/vda mklabel gpt mkpart BOOT 1MiB 2MiB set 1 bios_grub on mkpart NIXOS btrfs 2MiB 100%
+    mknod /dev/btrfs-control c 10 234
+    mkfs.btrfs /dev/vda2
+    mkdir /fsroot && mount /dev/vda2 /fsroot
+    btrfs subvol create /fsroot/boot
+    btrfs subvol create /fsroot/nix
+    btrfs subvol create /fsroot/persist
+    mkdir -p /mnt/{boot,nix}
+    mount -o subvol=boot,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/boot
+    mount -o subvol=nix,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/nix
     export NIX_STATE_DIR=$TMPDIR/state
-    ${nix}/bin/nix-store --load-db < ${db}/registration
-    ${nix}/bin/nix copy --no-check-sigs --to $root ${toplevel}
-    ${nix}/bin/nix-env --store $root -p $root/nix/var/nix/profiles/system --set ${toplevel}
-    ${fakeroot}/bin/fakeroot ${libguestfs-with-appliance}/bin/guestfish -N $out=fs:${rootfs.fsType}:2G -m /dev/sda1 << EOT
-    set-label /dev/sda1 ${rootfs.label}
-    copy-in $root/nix /
-    mkdir-mode /etc 0755
-    command "/nix/var/nix/profiles/system/activate"
-    command "/nix/var/nix/profiles/system/bin/switch-to-configuration boot"
-    EOT
-  '';
+    nix-store --load-db < ${db}/registration
+    nixos-install --root /mnt --system ${toplevel} --no-channel-copy --no-root-passwd --substituters ""
+  '');
 }
--- modules/vultr.nix
+++ modules/vultr.nix
@@ -7,19 +7,12 @@ in
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    loader.grub.device = "/dev/vda";
     initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
     kernel.sysctl = {
       "net.ipv6.conf.${ifname}.use_tempaddr" = 0;
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
     };
-  };
-
-  fileSystems."/" = {
-    label = "nixos";
-    fsType = "ext4";
-    autoResize = true;
   };
 
   networking = {
