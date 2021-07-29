892d04974b6d0242b64265972c159641452da425
Nick Cao <nickcao@nichi.co>
2021-07-29 13:33:41 +0800

    finalize migration

--- flake.nix
+++ flake.nix
@@ -105,17 +105,15 @@
         #     };
         #   };
         # };
-        /*
-          nrt0 = {
+        nrt0 = {
           sshUser = "root";
           hostname = "nrt0.nichi.link";
           profiles = {
-          system = {
-          path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nrt0;
+            system = {
+              path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nrt0;
+            };
           };
-          };
-          };
-        */
+        };
         sin0 = {
           sshUser = "root";
           hostname = "sin0.nichi.link";
