26203d123b3c27a8e324089f539313b14b060f5b
Nick Cao <nickcao@nichi.co>
2021-08-21 20:10:40 +0800

    firecracker-demo: get rid of stage2 init

--- pkgs/firecracker-demo/default.nix
+++ pkgs/firecracker-demo/default.nix
@@ -15,46 +15,40 @@
 }:
 let
   init = writeShellScript "init-stage1" ''
-    ${util-linux}/bin/mount -t tmpfs -o noatime,mode=0755 tmpfs /mnt
-    ${coreutils}/bin/mkdir -p /mnt/{root,work,upper}
-    ${util-linux}/bin/mount -o noatime,lowerdir=/,upperdir=/mnt/upper,workdir=/mnt/work -t overlay overlay /mnt/root
-    export PATH=${coreutils}/bin
-    ${util-linux}/bin/switch_root /mnt/root ${init-stage2}
+    ${util-linux}/bin/mount -t tmpfs -o noatime,mode=0755 tmpfs /tmp
+    ${coreutils}/bin/mkdir -p /tmp/{work,upper}
+    ${util-linux}/bin/mount -o noatime,lowerdir=/nix,upperdir=/tmp/upper,workdir=/tmp/work -t overlay overlay /nix
+    specialMount() {
+      local device="$1"
+      local mountPoint="$2"
+      local options="$3"
+      local fsType="$4"
+      ${util-linux}/bin/mount -n -t "$fsType" -o "$options" "$device" "$mountPoint"
+    }
+    specialMount "devtmpfs" "/dev" "nosuid,strictatime,mode=755,size=5%" "devtmpfs"
+    ${coreutils}/bin/mkdir /dev/pts
+    specialMount "devpts" "/dev/pts" "nosuid,noexec,mode=620,ptmxmode=0666,gid=3" "devpts"
+    specialMount "proc" "/proc" "nosuid,noexec,nodev" "proc"
+    specialMount "sysfs" "/sys" "nosuid,noexec,nodev" "sysfs"
+    ${nixUnstable}/bin/nix --experimental-features nix-command show-config
+    ${sirius}/bin/agent -p 1 -n ${nixUnstable}/bin/nix-daemon
   '';
   nix-config = writeText "nix.conf" ''
     build-users-group =
     trusted-users =
     substituters =
   '';
-  init-stage2 = writeShellScript "init-stage2" ''
-    specialMount() {
-      local device="$1"
-      local mountPoint="$2"
-      local options="$3"
-      local fsType="$4"
-      ${coreutils}/bin/mkdir -m 0755 -p "$mountPoint"
-      ${util-linux}/bin/mount -n -t "$fsType" -o "$options" "$device" "$mountPoint"
-    }
-    specialMount "devtmpfs" "/dev" "nosuid,strictatime,mode=755,size=5%" "devtmpfs"
-    specialMount "devpts" "/dev/pts" "nosuid,noexec,mode=620,ptmxmode=0666,gid=3" "devpts"
-    specialMount "proc" "/proc" "nosuid,noexec,nodev" "proc"
-    specialMount "sysfs" "/sys" "nosuid,noexec,nodev" "sysfs"
-    ${coreutils}/bin/mkdir -p /etc/nix
-    ${coreutils}/bin/echo "root:x:0:0::/root:" > /etc/passwd
-    ${coreutils}/bin/cp ${nix-config} /etc/nix/nix.conf
-    ${coreutils}/bin/mkdir /tmp
-    ${nixUnstable}/bin/nix --experimental-features nix-command show-config
-    ${sirius}/bin/agent -p 1 -n ${nixUnstable}/bin/nix-daemon
-  '';
   db = closureInfo { rootPaths = [ init ]; };
   image = runCommand "nixos.img"
     {
       requiredSystemFeatures = [ "recursive-nix" ];
-      nativeBuildInputs = [ e2fsprogs util-linux nixUnstable ];
+      nativeBuildInputs = [ e2fsprogs util-linux coreutils nixUnstable ];
     } ''
     touch $out
     truncate -s $(( $(cat ${db}/total-nar-size) + 500000000 )) $out
-    mkdir -p rootfs/mnt
+    mkdir -p rootfs/{tmp,dev,proc,sys,etc/nix}
+    echo "root:x:0:0::/:" > rootfs/etc/passwd
+    cp ${nix-config} rootfs/etc/nix/nix.conf
     nix --experimental-features nix-command copy --no-check-sigs --to ./rootfs ${init}
     mkfs.ext4 -d rootfs $out
   '';
