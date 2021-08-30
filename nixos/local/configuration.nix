709f67607c1923841783a3dd998f19bd9c0c6512
Nick Cao <nickcao@nichi.co>
2021-08-30 14:15:28 +0800

    local: optimize smartdns config

--- nixos/local/configuration.nix
+++ nixos/local/configuration.nix
@@ -163,21 +163,22 @@
     smartdns = {
       enable = true;
       settings = with pkgs; {
+        log-level = "info";
+        speed-check-mode = "none";
         conf-file = [
           "${smartdns-china-list}/accelerated-domains.china.smartdns.conf"
           "${smartdns-china-list}/apple.china.smartdns.conf"
           "${smartdns-china-list}/google.china.smartdns.conf"
         ];
-        nameserver =
-          [ "/cache.nixos.org/china" "/.6in4.dev/china" ];
         bind = [ "127.0.0.53:53" ];
+        server-https = [
+          "https://1.0.0.1/dns-query"
+          "https://1.1.1.1/dns-query"
+          "https://185.222.222.222/dns-query"
+        ];
         server = [
           "127.0.0.1 -group china -exclude-default-group"
           "2a0c:b641:69c:7864:0:5:0:3"
-        ];
-        server-https = [
-          "https://223.5.5.5/dns-query -group china -exclude-default-group"
-          "https://101.6.6.6:8443/dns-query"
         ];
       };
     };
