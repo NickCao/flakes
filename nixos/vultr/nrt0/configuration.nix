0c4bff2fb1bf0857cbdbaa4d32c30a9f14ece894
Nick Cao <nickcao@nichi.co>
2021-09-10 00:06:27 +0800

    vultr: bootstrap nomad cluster

--- nixos/vultr/nrt0/configuration.nix
+++ nixos/vultr/nrt0/configuration.nix
@@ -44,8 +44,26 @@
     interface.advertise = "enp1s0";
     extraConfig = {
       server = true;
+      datacenter = "global";
       bootstrap_expect = 3;
       retry_join = [ "nrt0.nichi.link" "sin0.nichi.link" "sea0.nichi.link" ];
+    };
+  };
+  services.nomad = {
+    enable = true;
+    enableDocker = false;
+    settings = {
+      datacenter = "nrt";
+      client = {
+        enabled = true;
+      };
+      server = {
+        enabled = true;
+        bootstrap_expect = 3;
+      };
+      consul = {
+        server_auto_join = true;
+      };
     };
   };
 }
--- nixos/vultr/sea0/configuration.nix
+++ nixos/vultr/sea0/configuration.nix
@@ -19,8 +19,26 @@
     interface.advertise = "enp1s0";
     extraConfig = {
       server = true;
+      datacenter = "global";
       bootstrap_expect = 3;
       retry_join = [ "nrt0.nichi.link" "sin0.nichi.link" "sea0.nichi.link" ];
+    };
+  };
+  services.nomad = {
+    enable = true;
+    enableDocker = false;
+    settings = {
+      datacenter = "sea";
+      client = {
+        enabled = true;
+      };
+      server = {
+        enabled = true;
+        bootstrap_expect = 2;
+      };
+      consul = {
+        server_auto_join = true;
+      };
     };
   };
 }
