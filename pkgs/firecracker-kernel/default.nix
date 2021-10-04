93a7ab6b1a89c154dcee342f7088ac47723b05ba
Nick Cao <nickcao@nichi.co>
2021-10-04 12:27:59 +0800

    firecracker-kernel: disable ifd

--- pkgs/firecracker-kernel/default.nix
+++ pkgs/firecracker-kernel/default.nix
@@ -2,7 +2,7 @@
 linuxManualConfig {
   inherit stdenv lib;
   inherit (linux_4_19) version src;
-  allowImportFromDerivation = true;
+  allowImportFromDerivation = false;
   configfile = fetchurl {
     url = "https://raw.githubusercontent.com/firecracker-microvm/firecracker/200c2db7055e406125729c40e14acf47024c1420/resources/microvm-kernel-x86_64.config";
     sha256 = "sha256-AEbQtRD7cWWJIzynEorT35wrfZrHnbSJ8a0wkC8wliE=";
