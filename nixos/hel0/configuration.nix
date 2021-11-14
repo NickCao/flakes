3394b30b7e6c39e8b70ed693203503611da9504e
Nick Cao <nickcao@nichi.co>
2021-11-14 23:05:45 +0800

    inputs: bump

--- nixos/hel0/configuration.nix
+++ nixos/hel0/configuration.nix
@@ -34,6 +34,12 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpzrZLU0peDu1otGtP2GcCeQIkI8kmfHjnwpbfpWBkv"
   ];
+  users.users.backup = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNPLArhyazrFjK4Jt/ImHSzICvwKOk4f+7OEcv2HEb7"
+    ];
+  };
 
   services.openssh.enable = true;
 
