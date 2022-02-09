d88792e6c2c241fe2f0d07cd89332c70602f9d69
Nick Cao <nickcao@nichi.co>
2022-02-09 16:47:11 +0800

    sea0: minor cleanup

--- nixos/vultr/sea0/default.nix
+++ nixos/vultr/sea0/default.nix
@@ -2,18 +2,17 @@
 nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
-    ./configuration.nix
     self.nixosModules.vultr
     self.nixosModules.telegraf
     self.nixosModules.ss
     self.nixosModules.cloud.common
     self.nixosModules.cloud.services
-    {
-      nixpkgs.overlays = [
-        self.overlay
-      ];
-    }
     inputs.sops-nix.nixosModules.sops
     inputs.impermanence.nixosModules.impermanence
+    ({ pkgs, config, ... }: {
+      nixpkgs.overlays = [ self.overlay ];
+      networking.hostName = "sea0";
+      services.dns.secondary.enable = true;
+    })
   ];
 }
