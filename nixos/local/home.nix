4669417ba16920eed0e7428f9d4dcde10d3c199f
Nick Cao <nickcao@nichi.co>
2022-02-12 16:47:21 +0800

    local: foot: use solarized dark theme

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -311,7 +311,6 @@ in
         set -g mouse on
         set -g status-right ""
         set -gs escape-time 10
-        set -g default-terminal "tmux-256color"
         set -g renumber-windows on
         set -g base-index 1
         new-session -s main
@@ -325,27 +324,29 @@ in
           font = "JetBrains Mono:size=10";
         };
         cursor = {
-          color = "fdf6e3 586e75";
+          color = "002b36 93a1a1";
         };
         colors = {
-          background = "fdf6e3";
-          foreground = "657b83";
-          regular0 = "eee8d5";
+          background = "002b36";
+          foreground = "839496";
+          regular0 = "073642";
           regular1 = "dc322f";
           regular2 = "859900";
           regular3 = "b58900";
           regular4 = "268bd2";
           regular5 = "d33682";
           regular6 = "2aa198";
-          regular7 = "073642";
-          bright0 = "cb4b16";
-          bright1 = "fdf6e3";
-          bright2 = "93a1a1";
-          bright3 = "839496";
-          bright4 = "657b83";
+          regular7 = "eee8d5";
+          bright0 = "002b36";
+          bright1 = "cb4b16";
+          bright2 = "586e75";
+          bright3 = "657b83";
+          bright4 = "839496";
           bright5 = "6c71c4";
-          bright6 = "586e75";
-          bright7 = "002b36";
+          bright6 = "93a1a1";
+          bright7 = "fdf6e3";
+          selection-foreground = "93a1a1";
+          selection-background = "073642";
         };
       };
     };
