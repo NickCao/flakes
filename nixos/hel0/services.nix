37962769f1d4a8cfffab3f8b2301a1390399b12a
Nick Cao <nickcao@nichi.co>
2022-03-03 17:13:43 +0800

    hel0: mautrix-telegram: add bot token

--- nixos/hel0/services.nix
+++ nixos/hel0/services.nix
@@ -121,7 +121,6 @@
         delivery_error_reports = true;
         sync_direct_chats = true;
         inline_images = true;
-        federate_rooms = false;
         tag_only_on_create = false;
         bridge_matrix_leave = false;
       };
