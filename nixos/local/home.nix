51e74660e3765920c1ef8030d8ac38aa7cd0b605
Nick Cao <nickcao@nichi.co>
2022-04-03 15:11:04 +0800

    local: fish: use standard terminal color

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -309,31 +309,6 @@ in
           fish_vi_key_bindings
           bind f accept-autosuggestion
         end
-        set fish_color_normal normal
-        set fish_color_command 93a1a1
-        set fish_color_quote 657b83
-        set fish_color_redirection 6c71c4
-        set fish_color_end 268bd2
-        set fish_color_error dc322f
-        set fish_color_param 839496
-        set fish_color_comment 586e75
-        set fish_color_match --background=brblue
-        set fish_color_selection white --bold --background=brblack
-        set fish_color_search_match bryellow --background=black
-        set fish_color_history_current --bold
-        set fish_color_operator 00a6b2
-        set fish_color_escape 00a6b2
-        set fish_color_cwd green
-        set fish_color_cwd_root red
-        set fish_color_valid_path --underline
-        set fish_color_autosuggestion 586e75
-        set fish_color_user brgreen
-        set fish_color_host normal
-        set fish_color_cancel -r
-        set fish_pager_color_completion B3A06D
-        set fish_pager_color_description B3A06D
-        set fish_pager_color_prefix cyan --underline
-        set fish_pager_color_progress brwhite --background=cyan
       '';
       shellAliases = {
         b = "brightnessctl";
