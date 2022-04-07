cddd5bf49067fa7bde780b6234050e5ef4b08653
Nick Cao <nickcao@nichi.co>
2022-04-07 11:23:31 +0800

    inputs: bump

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:NickCao/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
