883b871ae9436bc5362e3d165c6ddbbd4c705831
Nick Cao <nickcao@nichi.co>
2022-04-09 23:31:42 +0800

    jtag-remote-server: init

---
+++ pkgs/jtag-remote-server/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchFromGitHub, cmake, pkg-config, libftdi1 }:
+
+stdenv.mkDerivation rec {
+  pname = "jtag-remote-server";
+  version = "c27836164d6d3a5728666c6ebff3395cc7b194cc";
+
+  src = fetchFromGitHub {
+    owner = "jiegec";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-abHHMlhRyYwCKVQsZNIwbvC3oKdIIOAda3wuZ/ecl4c=";
+  };
+
+  nativeBuildInputs = [ cmake pkg-config ];
+  buildInputs = [ libftdi1 ];
+
+  installPhase = ''
+    install -Dm755 jtag-remote-server $out/bin/jtag-remote-server
+  '';
+}
