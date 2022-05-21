0139f81a9fefda57760967c81a8131bc5b4ddbbd
Nick Cao <nickcao@nichi.co>
2022-05-21 18:09:38 +0800

    hel0: matrix: begin migration from dendrite to synapse

--- nixos/hel0/matrix.nix
+++ nixos/hel0/matrix.nix
@@ -3,6 +3,7 @@
   sops = {
     secrets = {
       mautrix-telegram = { };
+      matrix-synapse = { owner = "matrix-synapse"; };
       matrix = { };
       matterbridge = { };
     };
@@ -13,107 +14,51 @@
   services.postgresql.authentication = ''
     local dendrite dendrite peer
     local mautrix-telegram mautrix-telegram peer
+    local matrix-synapse matrix-synapse peer
   '';
-
-  systemd.services.dendrite.serviceConfig.LoadCredential = [
-    "matrix:${config.sops.secrets.matrix.path}"
-    "mautrix-telegram:/var/lib/mautrix-telegram/telegram-registration.yaml"
-  ];
 
   cloud.services.element-web.config = {
     ExecStart = "${pkgs.serve}/bin/serve -l 127.0.0.1:8005 -p ${pkgs.element-web}";
   };
 
-  systemd.services.dendrite.after = [ "postgresql.service" ];
-  services.dendrite =
-    let
-      database = {
-        connection_string = "postgres:///dendrite?host=/run/postgresql";
-        max_open_conns = 20;
-      };
-    in
-    {
-      enable = true;
-      httpAddress = "127.0.0.1:8008";
-      settings = {
-        global = {
-          server_name = "nichi.co";
-          private_key = "/$CREDENTIALS_DIRECTORY/matrix";
-          jetstream = {
-            storage_path = "$STATE_DIRECTORY/nats";
-          };
-          presence = {
-            enable_inbound = true;
-            enable_outbound = true;
-          };
-        };
-        logging = [{
-          type = "std";
-          level = "warn";
-        }];
-        app_service_api = {
-          inherit database;
-          disable_tls_validation = true;
-          config_files = [
-            "/$CREDENTIALS_DIRECTORY/mautrix-telegram"
-          ];
-        };
-        client_api = {
-          registration_disabled = true;
-          rate_limiting.enabled = false;
-        };
-        media_api = {
-          inherit database;
-          max_file_size_bytes = 104857600;
-          dynamic_thumbnails = true;
-        };
-        room_server = {
-          inherit database;
-        };
-        push_server = {
-          inherit database;
-        };
-        mscs = {
-          inherit database;
-          mscs = [ "msc2444" "msc2753" "msc2836" "msc2946" ];
-        };
-        sync_api = {
-          inherit database;
-          real_ip_header = "X-Real-IP";
-        };
-        key_server = {
-          inherit database;
-        };
-        federation_api = {
-          inherit database;
-          key_perspectives = [{
-            server_name = "matrix.org";
-            keys = [
-              {
-                key_id = "ed25519:auto";
-                public_key = "Noi6WqcDj0QmPxCNQqgezwTlBKrfqehY1u2FyWP9uYw";
-              }
-              {
-                key_id = "ed25519:a_RXGa";
-                public_key = "l8Hft5qXKn1vfHrg3p4+W8gELQVo8N13JkluMfmn2sQ";
-              }
-            ];
-          }];
-        };
-        user_api = {
-          account_database = database;
-          device_database = database;
-        };
-      };
+  systemd.services.matrix-synapse.serviceConfig.LoadCredential = [
+    "telegram:/var/lib/mautrix-telegram/telegram-registration.yaml"
+  ];
+  services.matrix-synapse = {
+    enable = true;
+    settings = {
+      app_service_config_files = [ "/run/credentials/matrix-synapse.service/telegram" ];
+      listeners = [{
+        bind_addresses = [ "127.0.0.1" ];
+        port = 8196;
+        resources = [
+          {
+            compress = true;
+            names = [ "client" ];
+          }
+          {
+            compress = false;
+            names = [ "federation" ];
+          }
+        ];
+        tls = false;
+        type = "http";
+        x_forwarded = true;
+      }];
+      server_name = "nichi.co";
+      public_baseurl = "https://matrix.nichi.co";
+      dynamic_thumbnails = true;
+      signing_key_path = config.sops.secrets.matrix-synapse.path;
     };
+  };
 
   services.mautrix-telegram = {
     enable = true;
     environmentFile = config.sops.secrets.mautrix-telegram.path;
-    serviceDependencies = [ "dendrite.service" ];
+    serviceDependencies = [ "matrix-synapse.service" ];
     settings = {
       homeserver = {
-        address = "http://${config.services.dendrite.httpAddress}";
+        address = "http://127.0.0.1:8196";
         domain = "nichi.co";
         enablePresence = false;
       };
@@ -227,7 +172,7 @@
       matrix = {
         rule = "Host(`matrix.nichi.co`) && PathPrefix(`/_matrix`)";
         entryPoints = [ "https" ];
-        service = "dendrite";
+        service = "synapse";
       };
       element = {
         rule = "Host(`matrix.nichi.co`)";
@@ -239,9 +184,9 @@
       element.loadBalancer = {
         servers = [{ url = "http://127.0.0.1:8005"; }];
       };
-      dendrite.loadBalancer = {
+      synapse.loadBalancer = {
         passHostHeader = true;
-        servers = [{ url = "http://${config.services.dendrite.httpAddress}"; }];
+        servers = [{ url = "http://127.0.0.1:8196"; }];
       };
     };
   };
