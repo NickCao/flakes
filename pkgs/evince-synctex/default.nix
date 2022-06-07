b579093671fedf15d58adedc31b9b3c7457f0335
Nick Cao <nickcao@nichi.co>
2022-06-07 11:22:40 +0800

    pkgs: evince-synctex: patch for proper exit

--- pkgs/evince-synctex/default.nix
+++ pkgs/evince-synctex/default.nix
@@ -2,5 +2,6 @@
 
 python3Packages.buildPythonApplication {
   inherit (source) pname version src;
+  patches = [ ./poll.patch ];
   propagatedBuildInputs = with python3Packages; [ dbus-python pygobject3 ];
 }
