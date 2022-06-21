2d589c12fb094d9d8fc9fcef6fd35d67ea71c2b1
Nick Cao <nickcao@nichi.co>
2022-06-21 23:25:22 +0800

    Revert "hel0: postgresql: add timescaledb extension"

--- nixos/hel0/configuration.nix
+++ nixos/hel0/configuration.nix
@@ -35,7 +35,6 @@
 
   services.postgresql = {
     package = pkgs.postgresql_14;
-    extraPlugins = with pkgs.postgresql_14.pkgs; [ timescaledb ];
     settings = {
       max_connections = 300;
       shared_buffers = "16GB";
