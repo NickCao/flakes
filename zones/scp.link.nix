bd00575ad49643621b0fcf584c08309f8917bab9
Nick Cao <nickcao@nichi.co>
2022-06-29 10:30:21 +0800

    zones: dedup he nameservers

--- zones/scp.link.nix
+++ zones/scp.link.nix
@@ -2,12 +2,13 @@
 with dns.lib.combinators;
 let
   common = import ./common.nix;
+  he = builtins.map (x: "ns${builtins.toString x}.he.net.") (builtins.genList (x: x + 1) 5);
 in
 dns.lib.toString "scp.link" {
   inherit (common) TTL SOA NS;
   subdomains = {
-    "o".NS = builtins.map (x: "ns${builtins.toString x}.he.net.") (builtins.genList (x: x + 1) 5);
     vanilla.A = [ "20.24.195.187" ];
-    "com".NS = builtins.map (x: "ns${builtins.toString x}.he.net.") (builtins.genList (x: x + 1) 5);
+    "o".NS = he;
+    "com".NS = he;
   };
 }
