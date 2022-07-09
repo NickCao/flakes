0b12412bc7faadd755c8a88403fa73b598dc6188
Nick Cao <nickcao@nichi.co>
2022-07-09 20:48:23 +0800

    pkgs: make use of writeShellApplication

--- pkgs/mode/default.nix
+++ pkgs/mode/default.nix
@@ -1,20 +1,21 @@
-{ writeShellScriptBin, xxd, upower, gnugrep, systemd }:
-writeShellScriptBin "mode" ''
-  ECREG=`sudo ${xxd}/bin/xxd -s 0x1d -l 1 -ps /sys/kernel/debug/ec/ec0/io`
-  echo -n "Fan Control: "
-  case $ECREG in
-    00)
-      echo "Balanced Mode";;
-    01)
-      echo "Beast Mode";;
-    02)
-      echo "Quiet Mode";;
-  esac
-
-  echo -n "Freq Governor: "
-  cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-  echo -n "GPU Status: "
-  cat /sys/bus/pci/devices/0000:01:00.0/power/runtime_status
-
-  ${systemd}/lib/systemd/systemd-boot-check-no-failures
-''
+{ writeShellApplication, xxd }:
+writeShellApplication {
+  name = "mode";
+  runtimeInputs = [ xxd ];
+  text = ''
+    ECREG=$(sudo xxd -s 0x1d -l 1 -ps /sys/kernel/debug/ec/ec0/io)
+    echo -n "Fan Control: "
+    case $ECREG in
+      00)
+        echo "Balanced Mode";;
+      01)
+        echo "Beast Mode";;
+      02)
+        echo "Quiet Mode";;
+    esac
+    echo -n "Freq Governor: "
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo -n "GPU Status: "
+    cat /sys/bus/pci/devices/0000:01:00.0/power/runtime_status
+  '';
+}
