e3b0360e243bd7dfed65985d8addc754d65c749f
Nick Cao <nickcao@nichi.co>
2022-07-21 19:01:51 +0800

    hel0: git: add additional pubkey

--- nixos/hel0/git.nix
+++ nixos/hel0/git.nix
@@ -50,6 +50,7 @@ in
     shell = "${pkgs.git}/bin/git-shell";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNPLArhyazrFjK4Jt/ImHSzICvwKOk4f+7OEcv2HEb7"
     ];
   };
 
