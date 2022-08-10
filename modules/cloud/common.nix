c9a489e938413e7645370dd29e8ca5daf2a8b738
Nick Cao <nickcao@nichi.co>
2022-08-10 12:39:04 +0800

    modules: cloud: remove unused disk image generation

--- modules/cloud/common.nix
+++ modules/cloud/common.nix
@@ -44,10 +44,6 @@ in
   };
 
   boot = {
-    postBootCommands = ''
-      ${gptfdisk}/bin/sgdisk -e -d 2 -n 2:0:0 -c 2:NIXOS -p /dev/vda
-      ${util-linux}/bin/partx -u /dev/vda
-    '';
     tmpOnTmpfs = true;
     loader.grub.device = "/dev/vda";
     kernel.sysctl = {
@@ -95,31 +91,6 @@ in
     options = [ "subvol=persist" "noatime" "compress-force=zstd" "space_cache=v2" ];
     neededForBoot = true;
   };
-
-  system.build.image = vmTools.runInLinuxVM (runCommand "image"
-    {
-      preVM = ''
-        mkdir $out
-        diskImage=$out/nixos.img
-        ${vmTools.qemu}/bin/qemu-img create -f raw $diskImage $(( $(cat ${db}/total-nar-size) + 500000000 ))
-      '';
-      nativeBuildInputs = [ gptfdisk btrfs-progs mount util-linux nixUnstable config.system.build.nixos-install ];
-    } ''
-    sgdisk -Z -n 1:0:+1M -n 2:0:0 -t 1:ef02 -c 1:BOOT -c 2:NIXOS /dev/vda
-    mknod /dev/btrfs-control c 10 234
-    mkfs.btrfs /dev/vda2
-    mkdir /fsroot && mount /dev/vda2 /fsroot
-    btrfs subvol create /fsroot/boot
-    btrfs subvol create /fsroot/nix
-    btrfs subvol create /fsroot/persist
-    mkdir -p /mnt/{boot,nix,persist}
-    mount -o subvol=boot,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/boot
-    mount -o subvol=nix,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/nix
-    mount -o subvol=persist,compress-force=zstd,space_cache=v2 /dev/vda2 /mnt/persist
-    export NIX_STATE_DIR=$TMPDIR/state
-    nix-store --load-db < ${db}/registration
-    nixos-install --root /mnt --system ${toplevel} --no-channel-copy --no-root-passwd --substituters ""
-  '');
 
   environment.persistence."/persist" = {
     directories = [
