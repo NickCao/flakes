a6d859b6c0d510532eb0ea56b65a0dd9778efbed
Nick Cao <nickcao@nichi.co>
2022-10-03 10:26:19 +0800

    local: nvim: vim.lsp.buf.formatting -> vim.lsp.buf.format

--- nixos/local/nvim.lua
+++ nixos/local/nvim.lua
@@ -46,7 +46,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = false })<CR>', opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
