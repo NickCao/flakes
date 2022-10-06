51d7e373d45113bf96d89a77d7959038d9c7896f
Nick Cao <nickcao@nichi.co>
2022-10-06 18:05:12 +0800

    rpi: fix wpa supplicant config

--- nixos/rpi/configuration.nix
+++ nixos/rpi/configuration.nix
@@ -105,6 +105,7 @@ in
           proto=RSN
           pairwise=CCMP
           eap=PEAP
+          phase1="tls_disable_tlsv1_0=0"
           phase2="auth=MSCHAPV2"
           identity="@IDENTITY@"
           password="@PASSWORD@"
