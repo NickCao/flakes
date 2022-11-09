ef35658ad8c6e12342b734e1d094811861b946de
Nick Cao <nickcao@nichi.co>
2022-11-09 21:45:51 +0800

    hydra: import riscv jobset

--- infra/hydra.tf
+++ infra/hydra.tf
@@ -4,6 +4,49 @@ resource "hydra_project" "nixpkgs" {
   description  = "Nix Packages collection"
   homepage     = "https://nixos.org/nixpkgs"
   owner        = "nickcao@nichi.co"
-  enabled      = true
-  visible      = true
+}
+
+resource "hydra_jobset" "riscv" {
+  project = hydra_project.nixpkgs.name
+  state   = "enabled"
+  name    = "riscv"
+  type    = "legacy"
+
+  nix_expression {
+    file  = "pkgs/top-level/release.nix"
+    input = "nixpkgs"
+  }
+
+  check_interval    = 120
+  scheduling_shares = 100
+  keep_evaluations  = 2
+
+  input {
+    name              = "nixpkgs"
+    notify_committers = false
+    type              = "git"
+    value             = "https://github.com/NickCao/nixpkgs.git riscv"
+  }
+
+  input {
+    name              = "nixpkgsArgs"
+    notify_committers = false
+    type              = "nix"
+    value             = "{ config = { allowUnfree = false; inHydra = true; }; crossSystem.config = \"riscv64-unknown-linux-gnu\"; }"
+  }
+
+  input {
+    name              = "limitedSupportedSystems"
+    notify_committers = false
+    type              = "nix"
+    value             = "[]"
+  }
+
+  input {
+    name              = "supportedSystems"
+    notify_committers = false
+    type              = "nix"
+    value             = "[ \"x86_64-linux\" ]"
+  }
+
 }
