2efd2b5fc2f69235509c3c94a7224b9a9640048e
Nick Cao <nickcao@nichi.co>
2022-11-23 00:17:32 +0800

    infra: hcloud: assign public net

--- infra/hcloud.tf
+++ infra/hcloud.tf
@@ -5,4 +5,24 @@ resource "hcloud_server" "iad0" {
   image              = "debian-11"
   delete_protection  = true
   rebuild_protection = true
+  public_net {
+    ipv4 = hcloud_primary_ip.iad0_v4.id
+    ipv6 = hcloud_primary_ip.iad0_v6.id
+  }
+}
+
+resource "hcloud_primary_ip" "iad0_v4" {
+  name              = "iad0-v4"
+  type              = "ipv4"
+  assignee_type     = "server"
+  auto_delete       = false
+  delete_protection = true
+}
+
+resource "hcloud_primary_ip" "iad0_v6" {
+  name              = "iad0-v6"
+  type              = "ipv6"
+  assignee_type     = "server"
+  auto_delete       = false
+  delete_protection = true
 }
