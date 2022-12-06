ccf5c62adc538b3d6e733f5de3068af580d4240c
Nick Cao <nickcao@nichi.co>
2022-12-06 22:49:20 +0800

    hio0: deploy element web

--- nixos/hcloud/hio0/blog.nix
+++ nixos/hcloud/hio0/blog.nix
@@ -4,20 +4,49 @@
     ExecStart = "${pkgs.miniserve}/bin/miniserve -i 127.0.0.1 -p 8007 --index index.html ${pkgs.blog}";
   };
 
+  cloud.services.element-web.config =
+    let
+      conf = {
+        default_server_config = {
+          "m.homeserver" = {
+            base_url = "https://nichi.co";
+            server_name = "nichi.co";
+          };
+        };
+        brand = "Nichi Yorozuya";
+      };
+    in
+    {
+      ExecStart = "${pkgs.miniserve}/bin/miniserve -i 127.0.0.1 -p 8005 --index index.html ${pkgs.element-web.override { inherit conf; }}";
+    };
+
+
   services.traefik.dynamicConfigOptions.http = {
-    routers.blog = {
-      rule = "Host(`nichi.co`)";
-      entryPoints = [ "https" ];
-      middlewares = [ "blog" ];
-      service = "blog";
+    routers = {
+      blog = {
+        rule = "Host(`nichi.co`)";
+        entryPoints = [ "https" ];
+        middlewares = [ "blog" ];
+        service = "blog";
+      };
+      element = {
+        rule = "Host(`matrix.nichi.co`)";
+        entryPoints = [ "https" ];
+        service = "element";
+      };
     };
     middlewares.blog.headers = {
       stsSeconds = 31536000;
       stsIncludeSubdomains = true;
       stsPreload = true;
     };
-    services.blog.loadBalancer = {
-      servers = [{ url = "http://127.0.0.1:8007"; }];
+    services = {
+      blog.loadBalancer = {
+        servers = [{ url = "http://127.0.0.1:8007"; }];
+      };
+      element.loadBalancer = {
+        servers = [{ url = "http://127.0.0.1:8005"; }];
+      };
     };
   };
 
