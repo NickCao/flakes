7afd32369a20a10e768392a6b82fcb108db5e448
Nick Cao <nickcao@nichi.co>
2022-12-06 21:41:17 +0800

    hio0: init

---
+++ nixos/hcloud/hio0/default.nix
@@ -0,0 +1,59 @@
+{ pkgs, config, modulesPath, self, inputs, ... }: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    self.nixosModules.default
+    self.nixosModules.cloud.filesystems
+    inputs.impermanence.nixosModules.impermanence
+    inputs.sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    age = {
+      keyFile = "/var/lib/sops.key";
+      sshKeyPaths = [ ];
+    };
+    gnupg.sshKeyPaths = [ ];
+  };
+
+  nixpkgs.overlays = [ self.overlays.default ];
+
+  boot = {
+    loader.grub.device = "/dev/sda";
+    initrd.availableKernelModules = [ "ahci" "xhci_pci" "sd_mod" "sr_mod" ];
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      "/var"
+    ];
+  };
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+    fileSystems = [ "/persist" ];
+  };
+
+  networking = {
+    hostName = "hio0";
+    domain = "nichi.link";
+    useDHCP = false;
+    useNetworkd = true;
+    interfaces.enp1s0 = {
+      useDHCP = true;
+      ipv6.addresses = [{ address = ((import ../../../zones/common.nix).nodes.hio0.ipv6); prefixLength = 64; }];
+      ipv6.routes = [{ address = "::"; prefixLength = 0; via = "fe80::1"; }];
+    };
+  };
+
+  services.openssh.enable = true;
+  services.sshcert.enable = true;
+  services.gateway.enable = true;
+  services.metrics.enable = true;
+
+  users.users.root.openssh.authorizedKeys.keys = pkgs.keys;
+
+  environment.baseline.enable = true;
+
+  system.stateVersion = "22.05";
+}
