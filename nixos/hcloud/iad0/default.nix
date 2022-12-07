3971ecc2920b123de7e59de02201f37007257ee2
Nick Cao <nickcao@nichi.co>
2022-12-07 13:55:38 +0800

    iad0: use common config

--- nixos/hcloud/iad0/default.nix
+++ nixos/hcloud/iad0/default.nix
@@ -1,10 +1,7 @@
 { pkgs, config, modulesPath, self, inputs, data, ... }: {
+
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-    self.nixosModules.default
-    self.nixosModules.cloud.filesystems
-    inputs.impermanence.nixosModules.impermanence
-    inputs.sops-nix.nixosModules.sops
+    ../common.nix
     ./knot.nix
     ./postfix.nix
     ./dovecot.nix
@@ -12,66 +9,8 @@
     ./cgit.nix
   ];
 
-  sops = {
-    defaultSopsFile = ./secrets.yaml;
-    age = {
-      keyFile = "/var/lib/sops.key";
-      sshKeyPaths = [ ];
-    };
-    gnupg.sshKeyPaths = [ ];
-  };
+  sops.defaultSopsFile = ./secrets.yaml;
 
-  nixpkgs.overlays = [ self.overlays.default ];
+  networking.hostName = "iad0";
 
-  boot = {
-    loader.grub.device = "/dev/sda";
-    initrd.availableKernelModules = [ "ahci" "xhci_pci" "sd_mod" "sr_mod" ];
-  };
-
-  environment.persistence."/persist" = {
-    directories = [
-      "/var"
-    ];
-  };
-
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-    fileSystems = [ "/persist" ];
-  };
-
-  services.restic.backups = {
-    persist = {
-      repository = "sftp:backup:backup";
-      passwordFile = config.sops.secrets.restic.path;
-      paths = [ "/persist" ];
-      timerConfig = {
-        OnCalendar = "daily";
-      };
-    };
-  };
-
-  networking = {
-    hostName = "iad0";
-    domain = "nichi.link";
-    useDHCP = false;
-    useNetworkd = true;
-    interfaces.enp1s0 = {
-      useDHCP = true;
-      ipv6.addresses = [{ address = data.nodes.iad0.ipv6; prefixLength = 64; }];
-      ipv6.routes = [{ address = "::"; prefixLength = 0; via = "fe80::1"; }];
-    };
-  };
-
-  services.openssh.enable = true;
-  services.sshcert.enable = true;
-  services.gateway.enable = true;
-  services.metrics.enable = true;
-
-  users.users.root.openssh.authorizedKeys.keys = data.keys;
-
-  environment.baseline.enable = true;
-  environment.backup.enable = true;
-
-  system.stateVersion = "22.05";
 }
