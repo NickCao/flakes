8d9323d4267b6d87ce58e461a522b3bdbcb5359f
Nick Cao <nickcao@nichi.co>
2022-12-08 23:35:31 +0800

    shadowsocks: condition on secret availability

--- modules/shadowsocks/default.nix
+++ modules/shadowsocks/default.nix
@@ -4,8 +4,11 @@
     sopsFile = ./secrets.yaml;
     restartUnits = [ "shadowsocks.service" ];
   };
-  cloud.services.shadowsocks.config = {
-    ExecStart = "${pkgs.shadowsocks-rust}/bin/ssserver -c %d/config";
-    LoadCredential = "config:${config.sops.secrets.shadowsocks.path}";
+  cloud.services.shadowsocks = {
+    config = {
+      ExecStart = "${pkgs.shadowsocks-rust}/bin/ssserver -c %d/config";
+      LoadCredential = "config:${config.sops.secrets.shadowsocks.path}";
+    };
+    unit.ConditionFileNotEmpty = config.sops.secrets.shadowsocks.path;
   };
 }
