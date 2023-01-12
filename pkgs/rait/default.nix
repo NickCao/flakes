790d938eb18a63ad4dd8017b47b7d1dc112a5d56
Nick Cao <nickcao@nichi.co>
2023-01-12 13:20:10 +0800

    pkgs: bump

--- pkgs/rait/default.nix
+++ pkgs/rait/default.nix
@@ -1,7 +1,20 @@
-{ source, buildGoModule, lib }:
+{ lib
+, buildGoModule
+, fetchFromGitLab
+}:
 
 buildGoModule rec {
-  inherit (source) pname version src vendorSha256;
+  pname = "rait";
+  version = "4.4.0";
+
+  src = fetchFromGitLab {
+    owner = "NickCao";
+    repo = "RAIT";
+    rev = "v${version}";
+    hash = "sha256-6Y0s5/HUmWrZA6QmV5wYjB1M0Ab/jHM3TSruRpMRwtA=";
+  };
+
+  vendorHash = "sha256-T/ufC4mEXRBKgsmIk8jSCQva5Td0rnFHx3UIVV+t08k=";
 
   subPackages = [ "cmd/rait" ];
 
