f467fc0cbbecb17085f55781ecff038aeb127792
Nick Cao <nickcao@nichi.co>
2023-01-15 18:38:21 +0800

    hio0: canopus: make async

--- nixos/hcloud/hio0/services.nix
+++ nixos/hcloud/hio0/services.nix
@@ -10,23 +10,34 @@ in
     MemoryMax = "5G";
     SystemCallFilter = null;
     ExecStart = "${pkgs.python3.withPackages (ps: with ps;[ python-telegram-bot ])}/bin/python ${pkgs.writeText "canopus.py" ''
-      from telegram.ext import Updater
       from telegram import Update
-      from telegram.ext import CallbackContext
-      from telegram.ext import CommandHandler
-      import subprocess
+      from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+      import logging
+      import asyncio
       import os
-      updater = Updater(token=os.environ['BOT_TOKEN'])
-      dispatcher = updater.dispatcher
-      def eval(update: Update, context: CallbackContext):
+
+      logging.basicConfig(format="%(name)s [%(levelname)s] %(message)s", level=logging.INFO)
+      logger = logging.getLogger(__name__)
+
+      async def eval_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
           expr = "with import <nixpkgs> {};" + update.message.text.split(maxsplit=1)[-1]
           try:
-              res = subprocess.run(["${canopus}/bin/canopus", "${inputs.nixpkgs}", expr], capture_output=True, timeout=5)
-              context.bot.send_message(chat_id=update.effective_chat.id, text=res.stdout.decode("utf-8"))
+              proc = await asyncio.create_subprocess_exec("${canopus}/bin/canopus", "${inputs.nixpkgs}", expr,
+                  stderr=asyncio.subprocess.PIPE,
+                  stdout=asyncio.subprocess.PIPE)
+              stdout, stderr = await asyncio.wait_for(proc.communicate(), 5)
+              await update.message.reply_text(stdout.decode())
+          except asyncio.exceptions.TimeoutError:
+              proc.kill()
+              await update.message.reply_text("evaluation timed out")
           except Exception as e:
-              context.bot.send_message(chat_id=update.effective_chat.id, text="evaluation timed out")
-      dispatcher.add_handler(CommandHandler('eval', eval))
-      updater.start_polling()
+              logging.warning(e)
+              await update.message.reply_text("evaluation error")
+
+      token = os.environ['BOT_TOKEN']
+      application = ApplicationBuilder().token(token).build()
+      application.add_handler(CommandHandler('eval', eval_handler))
+      application.run_polling()
     ''}";
     EnvironmentFile = config.sops.secrets.canopus.path;
   };
