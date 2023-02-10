80fb0bc02fecf3db1614ee2c6ce93ab5518cc587
Nick Cao <nickcao@nichi.co>
2023-02-10 13:34:39 +0800

    local: set CARGO_UNSTABLE_SPARSE_REGISTRY

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -231,6 +231,7 @@ in
   ];
 
   systemd.user.sessionVariables = {
+    CARGO_UNSTABLE_SPARSE_REGISTRY = "true";
     LIBVA_DRIVER_NAME = "iHD";
     SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/resign.ssh";
     SOPS_GPG_EXEC = "resign-gpg";
