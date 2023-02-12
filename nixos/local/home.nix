195522885e88191fd3517ee8b93e5586a0ceb4c2
Nick Cao <nickcao@nichi.co>
2023-02-12 09:54:57 +0800

    local: firefox: install ubo

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -177,6 +177,10 @@ in
             installation_mode = "force_installed";
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           };
+          "uBlock0@raymondhill.net" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          };
         };
       };
     };
