396f0cbaa10401eebf036befe9a3133b2252a73c
Nick Cao <nickcao@nichi.co>
2023-03-14 18:09:08 +0800

    gravity: set mtu to 1420

--- modules/gravity/default.nix
+++ modules/gravity/default.nix
@@ -346,7 +346,7 @@ in
               case "$PLUTO_VERB" in
                 up-*)
                   ip link add "$LINK" type xfrm if_id "$PLUTO_IF_ID_OUT"
-                  ip link set "$LINK" master gravity multicast on up
+                  ip link set "$LINK" master gravity multicast on mtu 1420 up
                   ;;
                 down-*)
                   ip link del "$LINK"
