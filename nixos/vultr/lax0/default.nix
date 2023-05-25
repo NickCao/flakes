f172d72cba144f956754b8dc7bd1a17903973671
Nick Cao <nickcao@nichi.co>
2023-05-25 17:44:23 +0800

    lax0: use caddy instead of traefik

--- nixos/vultr/lax0/default.nix
+++ nixos/vultr/lax0/default.nix
@@ -1,10 +1,87 @@
-{ config, ... }:
+{ pkgs, lib, config, ... }:
+let
+  caddyConfig = (pkgs.formats.json { }).generate "config.json" {
+    admin = {
+      disabled = true;
+      config = {
+        persist = false;
+      };
+    };
+    logging = {
+      sink = {
+        writer = {
+          output = "stdout";
+        };
+      };
+      logs = {
+        default = {
+          level = "DEBUG";
+        };
+      };
+    };
+    apps = {
+      tls = {
+        automation = {
+          policies = [{
+            on_demand = true;
+            key_type = "p256";
+          }];
+        };
+      };
+      http = {
+        servers = {
+          default = {
+            listen = [ ":443" ];
+            routes = [
+              {
+                match = [{
+                  host = [ "ntfy.nichi.co" ];
+                }];
+                handle = [{
+                  handler = "reverse_proxy";
+                  upstreams = [{ dial = "unix/${config.services.ntfy-sh.settings.listen-unix}"; }];
+                }];
+              }
+              {
+                match = [{
+                  host = [ config.networking.fqdn ];
+                  path = [ "/prom" "/prom/*" ];
+                }];
+                handle = [{
+                  handler = "reverse_proxy";
+                  upstreams = [{ dial = "${config.services.prometheus.listenAddress}:${builtins.toString config.services.prometheus.port}"; }];
+                }];
+              }
+            ];
+          };
+        };
+      };
+    };
+  };
+in
 {
+
   imports = [
     ../common.nix
     ./prometheus.nix
     ./ntfy.nix
   ];
+
   sops.defaultSopsFile = ./secrets.yaml;
+
   networking.hostName = "lax0";
+
+  services.gateway.enable = lib.mkForce false;
+
+  systemd.services.caddy = {
+    serviceConfig = {
+      ExecStart = "${pkgs.caddy-nickcao}/bin/caddy run --adapter jsonc --config ${caddyConfig}";
+      DynamicUser = true;
+      StateDirectory = "caddy";
+      AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+      Environment = [ "XDG_DATA_HOME=%S" ];
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
 }
--- nixos/vultr/lax0/ntfy.nix
+++ nixos/vultr/lax0/ntfy.nix
@@ -4,20 +4,10 @@
     enable = true;
     settings = {
       base-url = "https://ntfy.nichi.co";
-      listen-http = "127.0.0.1:8008";
+      listen-http = "";
+      listen-unix = "/var/lib/ntfy-sh/ntfy.sock";
+      listen-unix-mode = 511; # 0777
       behind-proxy = true;
-    };
-  };
-
-  services.traefik.dynamicConfigOptions.http = {
-    routers.ntfy = {
-      rule = "Host(`ntfy.nichi.co`)";
-      entryPoints = [ "https" ];
-      service = "ntfy";
-    };
-    services.ntfy.loadBalancer = {
-      passHostHeader = true;
-      servers = [{ url = "http://${config.services.ntfy-sh.settings.listen-http}"; }];
     };
   };
 
