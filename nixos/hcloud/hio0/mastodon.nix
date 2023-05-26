6b36fef493f6c1deba78f20c7e29c68cacdc17f2
Nick Cao <nickcao@nichi.co>
2023-05-26 20:05:24 +0800

    hio0: expose mastodon

--- nixos/hcloud/hio0/mastodon.nix
+++ nixos/hcloud/hio0/mastodon.nix
@@ -12,4 +12,24 @@
     };
   };
 
+  systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "mastodon" ];
+
+  cloud.caddy.settings.apps.http.servers.default.routes = [{
+    match = [{
+      host = [ "mastodon.nichi.co" ];
+    }];
+    handle = [
+      {
+        handler = "file_server";
+        root = "${pkgs.mastodon}/public";
+        pass_thru = true;
+      }
+      {
+        handler = "reverse_proxy";
+        upstreams = [{ dial = "unix//run/mastodon-web/web.socket"; }];
+      }
+    ];
+  }];
+
+
 }
