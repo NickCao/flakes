9597fb57de7ada86a8d07c44afec2515d937b4a6
Nick Cao <nickcao@nichi.co>
2023-08-03 12:25:53 +0800

    itm0: enable gravity

--- nixos/vultr/itm0/default.nix
+++ nixos/vultr/itm0/default.nix
@@ -1,6 +1,33 @@
-{ ... }: {
+{ config, ... }: {
 
   imports = [ ../common.nix ];
+
   networking.hostName = "itm0";
+
+  services.gravity = {
+    enable = true;
+    reload.enable = true;
+    address = [ "2a0c:b641:69c:a4b0::1/128" ];
+    bird = {
+      enable = true;
+      exit.enable = true;
+      prefix = "2a0c:b641:69c:a4b0::/60";
+    };
+    divi = {
+      enable = true;
+      prefix = "2a0c:b641:69c:a4b4:0:4::/96";
+    };
+    ipsec = {
+      enable = true;
+      organization = "nickcao";
+      commonName = config.networking.hostName;
+      port = 13000;
+      interfaces = [ "ens3" ];
+      endpoints = [
+        { serialNumber = "0"; addressFamily = "ip4"; }
+        { serialNumber = "1"; addressFamily = "ip6"; }
+      ];
+    };
+  };
 
 }
