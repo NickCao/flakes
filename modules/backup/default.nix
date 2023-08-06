7e050f2ed43536f77102fda2f58c86037c750938
Nick Cao <nickcao@nichi.co>
2023-08-06 12:23:36 +0800

    modules/backup: add rsync.net host

--- modules/backup/default.nix
+++ modules/backup/default.nix
@@ -9,10 +9,21 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+
+    programs.ssh = {
+      extraConfig = ''
+        Host backup
+          HostName fm1622.rsync.net
+          User fm1622
+          IdentityFile ${config.sops.secrets.restic-keys.path}
+      '';
+    };
+
     sops.secrets = {
       restic-repo = { sopsFile = ./secrets.yaml; };
       restic-pass = { sopsFile = ./secrets.yaml; };
       restic-envs = { sopsFile = ./secrets.yaml; };
+      restic-keys = { sopsFile = ./secrets.yaml; };
     };
 
     environment.systemPackages = [ pkgs.restic ];
