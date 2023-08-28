070e591d0a8abd9c91fcd2c0a3a8f227e28d8a3a
Nick Cao <nickcao@nichi.co>
2023-08-28 09:26:34 +0800

    pkgs/caddy-nickcao: unstable-2023-08-07 -> unstable-2023-08-28

--- pkgs/caddy-nickcao/default.nix
+++ pkgs/caddy-nickcao/default.nix
@@ -4,16 +4,16 @@
 
 buildGoModule rec {
   pname = "caddy";
-  version = "unstable-2023-08-07";
+  version = "unstable-2023-08-28";
 
   src = fetchFromGitHub {
     owner = "NickCao";
     repo = "caddy";
-    rev = "8352ba4540efee4a086628ed8863b8c43e514b8f";
-    hash = "sha256-SQWNG4P8yx4jvTCVOEpGIH2g3C6uUQjbyy/lEzC3Hl8=";
+    rev = "d4997f9b8adb32c5e7d0cad70638c1e66e7c6da2";
+    hash = "sha256-ih3yuXzd4KHrg9BGM/RG6Iu7NBCsq4qPTf9ZcqrvEFQ=";
   };
 
-  vendorHash = "sha256-UPRJVUUKn9dbtLF3sq0XH445lHCEjRneVWqoHJTfxwY=";
+  vendorHash = "sha256-azPOec/kNK4y/0UstfaI/yLgZIyQmd1J++C19I/lRys=";
 
   subPackages = [ "cmd/caddy" ];
 
