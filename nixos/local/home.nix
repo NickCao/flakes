2fa46ab6d5d96eeaf50e91853f2d0111434e6898
Nick Cao <nickcao@nichi.co>
2023-09-18 20:59:31 -0400

    local: drop GOPROXY and GOSUMDB

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -433,8 +433,6 @@ in
       "go/env".text = ''
         GOPATH=${config.xdg.cacheHome}/go
         GOBIN=${config.xdg.stateHome}/go/bin
-        GOPROXY=https://goproxy.cn
-        GOSUMDB=sum.golang.google.cn
       '';
     };
   };
