05716205dec7b30da6b268950bba2b37b819b9af
Nick Cao <nickcao@nichi.co>
2023-09-24 10:21:20 -0400

    modules/dns/secondary: migrate to RFC42 settings

--- modules/dns/secondary/default.nix
+++ modules/dns/secondary/default.nix
@@ -15,50 +15,72 @@ in
     services.knot = {
       enable = true;
       keyFiles = [ config.sops.secrets.tsig.path ];
-      extraConfig = ''
-        server:
-          async-start: true
-          tcp-reuseport: true
-          tcp-fastopen: true
-          edns-client-subnet: true
-          listen: 0.0.0.0
-          listen: ::
+      settings = {
+        server = {
+          async-start = true;
+          tcp-reuseport = true;
+          tcp-fastopen = true;
+          edns-client-subnet = true;
+          listen = [
+            "0.0.0.0"
+            "::"
+          ];
+        };
 
-        log:
-          - target: syslog
-            any: info
+        log = [{
+          target = "syslog";
+          any = "info";
+        }];
 
-        remote:
-          - id: transfer
-            address: ${data.nodes.iad0.ipv4}
-            address: ${data.nodes.iad0.ipv6}
-            key: transfer
-          - id: cloudflare
-            address: 1.1.1.1
-            address: 1.0.0.1
-            address: 2606:4700:4700::1111
-            address: 2606:4700:4700::1001
+        remote = [
+          {
+            id = "transfer";
+            key = "transfer";
+            address = [
+              data.nodes.iad0.ipv4
+              data.nodes.iad0.ipv6
+            ];
+          }
+          {
+            id = "cloudflare";
+            address = [
+              "1.1.1.1"
+              "1.0.0.1"
+              "2606:4700:4700::1111"
+              "2606:4700:4700::1001"
+            ];
+          }
+        ];
 
-        mod-dnsproxy:
-          - id: cloudflare
-            remote: cloudflare
-            fallback: on
-            address: 2a0c:b641:69c::/48
-            address: 2001:470:4c22::/48
+        mod-dnsproxy = [{
+          id = "cloudflare";
+          remote = "cloudflare";
+          fallback = true;
+          address = [
+            "2a0c:b641:69c::/48"
+            "2001:470:4c22::/48"
+          ];
+        }];
 
-        template:
-          - id: default
-            global-module: mod-dnsproxy/cloudflare
-          - id: member
-            master: transfer
-            zonemd-verify: on
+        template = [
+          {
+            id = "default";
+            global-module = "mod-dnsproxy/cloudflare";
+          }
+          {
+            id = "member";
+            master = "transfer";
+            zonemd-verify = true;
+          }
+        ];
 
-        zone:
-          - domain: firstparty
-            master: transfer
-            catalog-role: interpret
-            catalog-template: member
-      '';
+        zone = [{
+          domain = "firstparty";
+          master = "transfer";
+          catalog-role = "interpret";
+          catalog-template = "member";
+        }];
+      };
     };
   };
 }
