fb5fba66cc6d6d80060a1e9109c8c927b2018a76
Nick Cao <nickcao@nichi.co>
2023-10-08 20:02:08 -0400

    modules/gravity: run updown script only once per child sa

--- modules/gravity/default.nix
+++ modules/gravity/default.nix
@@ -339,11 +339,11 @@ in
             updown = pkgs.writeShellScript "updown" ''
               LINK=gn$(printf '%08x\n' "$PLUTO_IF_ID_OUT")
               case "$PLUTO_VERB" in
-                up-*)
+                up-client)
                   ip link add "$LINK" type xfrm if_id "$PLUTO_IF_ID_OUT"
                   ip link set "$LINK" master gravity multicast on mtu 1420 up
                   ;;
-                down-*)
+                down-client)
                   ip link del "$LINK"
                   ;;
               esac
