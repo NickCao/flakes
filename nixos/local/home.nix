1218c6947e8dfb613931e41f0bfa28908449b34d
Nick Cao <nickcao@nichi.co>
2023-11-19 09:47:21 -0500

    inputs: bump

--- nixos/local/home.nix
+++ nixos/local/home.nix
@@ -143,39 +143,37 @@ in
   };
   programs.firefox = {
     enable = true;
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-      extraPolicies = {
-        PasswordManagerEnabled = false;
-        DisableFirefoxAccounts = true;
-        DisablePocket = true;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
+    policies = {
+      PasswordManagerEnabled = false;
+      DisableFirefoxAccounts = true;
+      DisablePocket = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      Proxy = {
+        Mode = "manual";
+        SOCKSProxy = "127.0.0.1:1080";
+        SOCKSVersion = 5;
+        UseProxyForDNS = true;
+      };
+      Preferences = {
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.urlbar.autoFill.adaptiveHistory.enabled" = true;
+        "browser.tabs.closeWindowWithLastTab" = false;
+        "extensions.unifiedExtensions.enabled" = false;
+      };
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
         };
-        Proxy = {
-          Mode = "manual";
-          SOCKSProxy = "127.0.0.1:1080";
-          SOCKSVersion = 5;
-          UseProxyForDNS = true;
-        };
-        Preferences = {
-          "browser.newtabpage.activity-stream.feeds.topsites" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.urlbar.autoFill.adaptiveHistory.enabled" = true;
-          "browser.tabs.closeWindowWithLastTab" = false;
-          "extensions.unifiedExtensions.enabled" = false;
-        };
-        ExtensionSettings = {
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            installation_mode = "force_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          };
-          "uBlock0@raymondhill.net" = {
-            installation_mode = "force_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          };
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
         };
       };
     };
