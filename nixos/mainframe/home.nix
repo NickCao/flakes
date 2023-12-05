f3205f9152092c5270e1df183efae15b1e2e295f
Nick Cao <nickcao@nichi.co>
2023-12-05 09:02:14 -0500

    mainframe: suspend instead of just lock

--- nixos/mainframe/home.nix
+++ nixos/mainframe/home.nix
@@ -421,11 +421,7 @@ in
       enable = true;
       timeouts = [
         { timeout = 900; command = "${pkgs.swaylock}/bin/swaylock"; }
-        {
-          timeout = 905;
-          command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"'';
-          resumeCommand = ''${pkgs.sway}/bin/swaymsg "output * dpms on"'';
-        }
+        { timeout = 905; command = "/run/current-system/systemd/bin/systemctl suspend"; }
       ];
       events = [
         { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock"; }
