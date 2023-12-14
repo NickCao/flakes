b201849f156e75f50beffccdf4da422d16c7068d
Nick Cao <nickcao@nichi.co>
2023-12-13 21:33:59 -0500

    mainframe: enable mitigations
    Reference: https://www.phoronix.com/review/amd-zen4-spectrev2

--- nixos/mainframe/configuration.nix
+++ nixos/mainframe/configuration.nix
@@ -130,7 +130,6 @@
     };
     kernelPackages = pkgs.linuxPackages_testing;
     kernelParams = [
-      "mitigations=off"
       "rtc_cmos.use_acpi_alarm=1"
       "amdgpu.sg_display=0"
       # "amdgpu.abmlevel=1"
