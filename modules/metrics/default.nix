2d0fc715d14cc7443215e9eed52660941c9c390b
Nick Cao <nickcao@nichi.co>
2024-02-21 11:30:12 -0500

    modules/metrics: add blackbox exporter

--- modules/metrics/default.nix
+++ modules/metrics/default.nix
@@ -22,27 +22,63 @@ in
       disabledCollectors = [ "arp" ];
     };
 
-    cloud.caddy.settings.apps.http.servers.default.routes = [{
-      match = [{
-        host = [ config.networking.fqdn ];
-        path = [ "/metrics" ];
-      }];
-      handle = [
-        {
-          handler = "authentication";
-          providers.http_basic.accounts = [{
-            username = "prometheus";
-            password = "{env.PROM_PASSWD}";
-          }];
-        }
-        {
-          handler = "reverse_proxy";
-          upstreams = with config.services.prometheus.exporters.node;[{
-            dial = "${listenAddress}:${toString port}";
-          }];
-        }
-      ];
-    }];
+    services.prometheus.exporters.blackbox = {
+      enable = true;
+      listenAddress = "127.0.0.1";
+      configFile = (pkgs.formats.yaml { }).generate "config.yml" {
+        modules = {
+          http_2xx = {
+            prober = "http";
+            http.preferred_ip_protocol = "ip6";
+          };
+        };
+      };
+    };
+
+    cloud.caddy.settings.apps.http.servers.default.routes = [
+      {
+        match = [{
+          host = [ config.networking.fqdn ];
+          path = [ "/metrics" ];
+        }];
+        handle = [
+          {
+            handler = "authentication";
+            providers.http_basic.accounts = [{
+              username = "prometheus";
+              password = "{env.PROM_PASSWD}";
+            }];
+          }
+          {
+            handler = "reverse_proxy";
+            upstreams = with config.services.prometheus.exporters.node;[{
+              dial = "${listenAddress}:${toString port}";
+            }];
+          }
+        ];
+      }
+      {
+        match = [{
+          host = [ config.networking.fqdn ];
+          path = [ "/probe" ];
+        }];
+        handle = [
+          {
+            handler = "authentication";
+            providers.http_basic.accounts = [{
+              username = "prometheus";
+              password = "{env.PROM_PASSWD}";
+            }];
+          }
+          {
+            handler = "reverse_proxy";
+            upstreams = with config.services.prometheus.exporters.blackbox;[{
+              dial = "${listenAddress}:${toString port}";
+            }];
+          }
+        ];
+      }
+    ];
 
   };
 }
