4d1e7af95278ea6a00e1551186e53e5c5037b5db
Nick Cao <nickcao@nichi.co>
2024-02-20 22:57:51 -0500

    treewide: drop vector due to it's massive closure size

--- modules/metrics.nix
+++ modules/metrics.nix
@@ -7,43 +7,6 @@ in
     enable = lib.mkEnableOption "export server metrics";
   };
   config = lib.mkIf cfg.enable {
-    services.vector = {
-      enable = true;
-      settings = {
-        sources = {
-          node = {
-            type = "prometheus_scrape";
-            endpoints = with config.services.prometheus.exporters.node;[
-              "http://${listenAddress}:${toString port}/metrics"
-            ];
-          };
-          blackbox = {
-            type = "prometheus_scrape";
-            endpoints = with config.services.prometheus.exporters.blackbox;[
-              "http://${listenAddress}:${toString port}/probe"
-            ];
-            query = {
-              module = [ "dns" ];
-              target = [ "1.0.0.1" ];
-            };
-          };
-        };
-        transforms = {
-          aggregated = {
-            type = "remap";
-            inputs = [ "node" "blackbox" ];
-            source = ".tags.host = \"${config.networking.hostName}\"";
-          };
-        };
-        sinks = {
-          prom = {
-            inputs = [ "aggregated" ];
-            type = "prometheus_exporter";
-            address = "127.0.0.1:9273";
-          };
-        };
-      };
-    };
 
     services.prometheus.exporters.node = {
       enable = true;
@@ -76,7 +39,9 @@ in
       }];
       handle = [{
         handler = "reverse_proxy";
-        upstreams = [{ dial = config.services.vector.settings.sinks.prom.address; }];
+        upstreams = with config.services.prometheus.exporters.node;[{
+          dial = "${listenAddress}:${toString port}";
+        }];
       }];
     }];
   };
