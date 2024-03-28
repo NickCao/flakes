dbb78f2ffd1fdc30d99cc0d2b38cab0e5687a50c
Nick Cao <nickcao@nichi.co>
2024-03-28 18:44:46 -0400

    fn: use stable Deno.serve api

--- fn/index.ts
+++ fn/index.ts
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 import { Router } from "npm:itty-router@3.0.11";
 import { rants } from "./rants.ts";
 
@@ -8,7 +7,7 @@ router.get("/rait", () => fetch(Deno.env.get("RAIT")));
 router.get("/rants/:id", rants);
 router.all("*", () => new Response("Not Found", { status: 404 }));
 
-await serve(router.handle, {
+Deno.serve({
   hostname: "127.0.0.1",
   port: parseInt(Deno.env.get("PORT") as string),
-});
+}, router.handle);
