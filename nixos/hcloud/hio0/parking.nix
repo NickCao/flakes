be1c32e2ccd2f2a2958389bd27d109809d408096
Nick Cao <nickcao@nichi.co>
2024-06-24 23:50:33 -0400

    hio0/parking: implement systemd.network style redirecting

--- nixos/hcloud/hio0/parking.nix
+++ nixos/hcloud/hio0/parking.nix
@@ -1,16 +1,15 @@
-{ ... }:
+{ lib, ... }:
 let
+  inherit (lib) singleton;
   mkRoute = host: location: {
-    match = [ { host = [ host ]; } ];
-    handle = [
-      {
-        handler = "static_response";
-        status_code = "302";
-        headers = {
-          Location = [ location ];
-        };
-      }
-    ];
+    match = singleton { host = singleton host; };
+    handle = singleton {
+      handler = "static_response";
+      status_code = "302";
+      headers = {
+        Location = singleton location;
+      };
+    };
   };
 in
 {
@@ -19,6 +18,32 @@ in
     (mkRoute "nixos.zip" "https://channels.nixos.org/nixos-unstable")
     (mkRoute "archlinux.icu" "https://manjaro.org")
     (mkRoute "nixos.icu" "https://archlinux.org")
-    (mkRoute "systemd.services" "https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html")
+    {
+      match = singleton { host = singleton "systemd.services"; };
+      handle = singleton {
+        handler = "subroute";
+        routes = [
+          {
+            match = singleton { path = singleton "/"; };
+            handle = singleton {
+              handler = "static_response";
+              status_code = "302";
+              headers = {
+                Location = singleton "/systemd.service";
+              };
+            };
+          }
+          {
+            handle = singleton {
+              handler = "static_response";
+              status_code = "302";
+              headers = {
+                Location = singleton "https://www.freedesktop.org/software/systemd/man/latest{http.request.uri.path}.html";
+              };
+            };
+          }
+        ];
+      };
+    }
   ];
 }
