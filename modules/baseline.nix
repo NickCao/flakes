b63578ae1ce61556173e9c44c452a1a14d4ee09d
Nick Cao <nickcao@nichi.co>
2024-08-16 19:29:12 -0400

    modules/baseline: raise UDP buffer sizes further

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -20,8 +20,9 @@ with lib;
         "kernel.panic" = 60;
         "net.core.default_qdisc" = "fq";
         "net.ipv4.tcp_congestion_control" = "bbr";
-        # https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size
-        "net.core.rmem_max" = 2500000;
+        # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
+        "net.core.rmem_max" = 7500000;
+        "net.core.wmem_max" = 7500000;
       };
     };
 
