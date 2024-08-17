91fae7927e0fe2ffb8a7e0faf689253e1b65a4cc
Nick Cao <nickcao@nichi.co>
2024-08-17 08:26:15 -0400

    hio0: permit insecure olm

--- nixos/hcloud/hio0/matrix.nix
+++ nixos/hcloud/hio0/matrix.nix
@@ -12,6 +12,11 @@ let
 in
 {
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "olm-3.2.16"
+    "jitsi-meet-1.0.8043"
+  ];
+
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_16;
