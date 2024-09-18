227e577c17eac17f61c8b7d4d4e449a2a05c13d4
Nick Cao <nickcao@nichi.co>
2024-09-17 20:33:53 -0400

    mainframe/fish: simplify theme setup

--- nixos/mainframe/home.nix
+++ nixos/mainframe/home.nix
@@ -256,32 +256,8 @@ in
         string replace -r '^' 'set -g ' < ${tide}/functions/tide/configure/configs/lean_16color.fish | source
         set -g tide_prompt_add_newline_before false
 
-        set fish_color_normal normal
-        set fish_color_command blue
-        set fish_color_quote yellow
-        set fish_color_redirection cyan --bold
-        set fish_color_end green
-        set fish_color_error brred
-        set fish_color_param cyan
-        set fish_color_comment red
-        set fish_color_match --background=brblue
-        set fish_color_selection white --bold --background=brblack
-        set fish_color_search_match bryellow --background=brblack
-        set fish_color_history_current --bold
-        set fish_color_operator brcyan
-        set fish_color_escape brcyan
-        set fish_color_cwd green
-        set fish_color_cwd_root red
-        set fish_color_valid_path --underline
+        fish_config theme choose fish\ default
         set fish_color_autosuggestion white
-        set fish_color_user brgreen
-        set fish_color_host normal
-        set fish_color_cancel --reverse
-        set fish_pager_color_prefix normal --bold --underline
-        set fish_pager_color_progress brwhite --background=cyan
-        set fish_pager_color_completion normal
-        set fish_pager_color_description B3A06D --italics
-        set fish_pager_color_selected_background --reverse
       '';
       shellAliases = {
         b = "brightnessctl";
