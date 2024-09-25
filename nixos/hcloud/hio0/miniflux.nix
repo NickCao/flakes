e64ed681a5341ed404c4e8b6b3a85db63ad0d443
Nick Cao <nickcao@nichi.co>
2024-09-25 09:34:37 -0400

    hio0/miniflux: drop admin user

--- nixos/hcloud/hio0/miniflux.nix
+++ nixos/hcloud/hio0/miniflux.nix
@@ -5,19 +5,24 @@ let
 in
 {
   sops.secrets.miniflux = {
-    restartUnits = [ "miniflux.service" ];
+    restartUnits = [ config.systemd.services.miniflux.name ];
   };
+
+  # https://miniflux.app/docs/howto.html#systemd-socket-activation
   systemd.sockets.miniflux = {
     wantedBy = [ "sockets.target" ];
-    requiredBy = [ "miniflux.service" ];
+    requiredBy = [ config.systemd.services.miniflux.name ];
     listenStreams = [ "/run/miniflux.sock" ];
   };
+
   systemd.services.miniflux.serviceConfig.NonBlocking = true;
+
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.sops.secrets.miniflux.path;
     config = {
       BASE_URL = baseURL;
+      CREATE_ADMIN = 0;
       OAUTH2_PROVIDER = "oidc";
       OAUTH2_CLIENT_ID = "miniflux";
       OAUTH2_REDIRECT_URL = "${baseURL}/oauth2/oidc/callback";
@@ -27,6 +32,7 @@ in
       SCHEDULER_ROUND_ROBIN_MIN_INTERVAL = 30;
     };
   };
+
   cloud.caddy.settings.apps.http.servers.default.routes = [
     {
       match = [ { host = [ hostName ]; } ];
