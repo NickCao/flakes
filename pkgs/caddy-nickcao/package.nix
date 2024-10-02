42527d220692f9d8c007aff99b0947bb60b08d5b
Nick Cao <nickcao@nichi.co>
2024-10-02 19:02:20 -0400

    pkgs/caddy-nickcao: 0-unstable-2024-08-13 -> 0-unstable-2024-10-02
    Diff: https://github.com/NickCao/caddy/compare/206d6345d6dd250c9769c0674340ba5e1909211f...62e39e55c1cfe3b288ac2f804dd3aab14fb1a9f9

--- pkgs/caddy-nickcao/package.nix
+++ pkgs/caddy-nickcao/package.nix
@@ -1,17 +1,17 @@
-{ buildGoModule, fetchFromGitHub }:
+{ buildGo123Module, fetchFromGitHub }:
 
-buildGoModule rec {
+buildGo123Module rec {
   pname = "caddy";
-  version = "0-unstable-2024-08-13";
+  version = "0-unstable-2024-10-02";
 
   src = fetchFromGitHub {
     owner = "NickCao";
     repo = "caddy";
-    rev = "206d6345d6dd250c9769c0674340ba5e1909211f";
-    hash = "sha256-PnO/xtKoKuHLTJc/7XiAF4K1jj8h8zesxLzHQ2qatkg=";
+    rev = "62e39e55c1cfe3b288ac2f804dd3aab14fb1a9f9";
+    hash = "sha256-5wNCB/goCTzVDby33xCfm3htuYjzAiEJisSR1GicW8A=";
   };
 
-  vendorHash = "sha256-JNOAIchcyyA0BhC3DGjpdd15Kl08C8EnF7ZIC62wycE=";
+  vendorHash = "sha256-6vd4aM9Kc4qH2MJBBh7DRHUnHO06KMrxLJvt7I5KP7A=";
 
   subPackages = [ "cmd/caddy" ];
 
