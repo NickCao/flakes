8742f5b83c103f3f4ff8fcccb0a26a5a4c306dc0
Nick Cao <nickcao@nichi.co>
2024-12-14 23:29:41 -0500

    fn/oproxy: set cache headers

--- fn/oproxy/src/main.rs
+++ fn/oproxy/src/main.rs
@@ -2,7 +2,10 @@ use anyhow::Result;
 use axum::{
     body::Body,
     extract::{Path, State},
-    http::StatusCode,
+    http::{
+        header::{self, InvalidHeaderValue, ToStrError},
+        HeaderMap, HeaderValue, StatusCode,
+    },
     response::IntoResponse,
     routing::get,
     Router,
@@ -18,6 +21,10 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 pub enum Error {
     #[error("opendal error")]
     Opendal(#[from] opendal::Error),
+    #[error("header error")]
+    Header(#[from] InvalidHeaderValue),
+    #[error("to str error")]
+    ToStr(#[from] ToStrError),
 }
 
 impl IntoResponse for Error {
@@ -28,10 +35,16 @@ impl IntoResponse for Error {
                     tracing::info!(error = err.to_string());
                     StatusCode::NOT_FOUND
                 }
+                ErrorKind::ConditionNotMatch => StatusCode::NOT_MODIFIED,
                 _ => {
                     tracing::error!(error = err.to_string());
                     StatusCode::INTERNAL_SERVER_ERROR
                 }
+            }
+            .into_response(),
+            _ => {
+                tracing::error!(error = self.to_string());
+                StatusCode::INTERNAL_SERVER_ERROR
             }
             .into_response(),
         }
@@ -40,11 +53,46 @@ impl IntoResponse for Error {
 
 async fn serve(
     State(operator): State<Arc<Operator>>,
+    headers: HeaderMap,
     Path(path): Path<String>,
 ) -> Result<impl IntoResponse, Error> {
-    let _ = operator.stat(&path).await?;
+    let metadata = if let Some(etag) = headers.get(header::IF_NONE_MATCH) {
+        operator
+            .stat_with(&path)
+            .if_none_match(etag.to_str()?)
+            .await?
+    } else {
+        operator.stat(&path).await?
+    };
+
     let reader = operator.reader(&path).await?;
-    Ok(Body::from_stream(reader.into_bytes_stream(..).await?))
+    let mut headers = HeaderMap::new();
+
+    for (k, v) in [
+        (header::CONTENT_TYPE, metadata.content_type()),
+        (
+            header::CONTENT_LENGTH,
+            Some(&metadata.content_length().to_string()),
+        ),
+        (header::ETAG, metadata.etag()),
+        (
+            header::CACHE_CONTROL,
+            Some("public, max-age=2419200, immutable"),
+        ),
+        (header::X_CONTENT_TYPE_OPTIONS, Some("nosniff")),
+        (
+            header::CONTENT_SECURITY_POLICY,
+            Some("default-src 'none'; form-action 'none'"),
+        ),
+    ] {
+        if let Some(v) = v {
+            headers.insert(k, HeaderValue::from_str(v)?);
+        }
+    }
+    Ok((
+        headers,
+        Body::from_stream(reader.into_bytes_stream(..).await?),
+    ))
 }
 
 #[derive(Parser, Debug)]
