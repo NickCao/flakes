ef3f670f8df822be36fcbded9925d1e7dcd6f3bb
Nick Cao <nickcao@nichi.co>
2025-01-11 10:46:40 -0500

    pkgs/bird-babel-rtt: 0-unstable-2024-12-08 -> 0-unstable-2025-01-11

--- pkgs/bird-babel-rtt/package.nix
+++ pkgs/bird-babel-rtt/package.nix
@@ -12,14 +12,14 @@
 stdenv.mkDerivation {
   pname = "bird-babel-rtt";
 
-  version = "0-unstable-2024-12-08";
+  version = "0-unstable-2025-01-11";
 
   src = fetchFromGitHub {
     owner = "NickCao";
     repo = "bird";
-    rev = "847dc444973ef16b0f3ed24660d010f1dd057589";
+    rev = "700a4fddd3b554cc1071608cf8d945400ae35c9e";
     fetchSubmodules = false;
-    sha256 = "sha256-+dOnj8RbZnpnDzyC51cmaLCD48xac72bRbttDCRLLqA=";
+    sha256 = "sha256-f6L/6S+GYCKVC5Pgj0TLD9R5C5+nj4Aczu8ztmWswCk=";
   };
 
   nativeBuildInputs = [
