65933186b87c008c772ae3885972a53951db827c
Nick Cao <nickcao@nichi.co>
2025-01-14 17:40:55 -0500

    infra: import keycloak clients

--- infra/keycloak.tf
+++ infra/keycloak.tf
@@ -2,3 +2,36 @@ resource "keycloak_realm" "nichi" {
   realm                       = "nichi"
   default_signature_algorithm = "RS256"
 }
+
+resource "keycloak_openid_client" "mastodon" {
+  realm_id    = keycloak_realm.nichi.id
+  client_id   = "mastodon"
+  name        = "Mastodon"
+  access_type = "CONFIDENTIAL"
+
+  base_url            = "https://mastodon.nichi.co"
+  valid_redirect_uris = ["https://mastodon.nichi.co/auth/auth/openid_connect/callback"]
+  web_origins         = ["https://mastodon.nichi.co"]
+}
+
+resource "keycloak_openid_client" "miniflux" {
+  realm_id    = keycloak_realm.nichi.id
+  client_id   = "miniflux"
+  name        = "Miniflux"
+  access_type = "CONFIDENTIAL"
+
+  base_url            = "https://rss.nichi.co"
+  valid_redirect_uris = ["https://rss.nichi.co/oauth2/oidc/callback"]
+  web_origins         = ["https://rss.nichi.co/oauth2/oidc/redirect"]
+}
+
+resource "keycloak_openid_client" "synapse" {
+  realm_id    = keycloak_realm.nichi.id
+  client_id   = "synapse"
+  name        = "Synapse"
+  access_type = "CONFIDENTIAL"
+
+  valid_redirect_uris    = ["https://matrix.nichi.co/_synapse/client/oidc/callback"]
+  web_origins            = ["https://matrix.nichi.co"]
+  backchannel_logout_url = "https://matrix.nichi.co/_synapse/client/oidc/backchannel_logout"
+}
