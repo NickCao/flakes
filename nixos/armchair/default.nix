8064fd80d89481209217b076a0bd503c495ae4ba
Nick Cao <nickcao@nichi.co>
2025-05-27 16:06:09 -0400

    Use flakes

---
+++ nixos/armchair/default.nix
@@ -0,0 +1,19 @@
+{
+  system,
+  nixpkgs,
+  inputs,
+}:
+
+nixpkgs.lib.nixosSystem {
+  inherit system;
+
+  modules = [
+    ./configuration.nix
+    ./hardware-configuration.nix
+    (inputs.nixos-apple-silicon.nixosModules.apple-silicon-support)
+  ];
+
+  specialArgs = {
+    inherit inputs;
+  };
+}
