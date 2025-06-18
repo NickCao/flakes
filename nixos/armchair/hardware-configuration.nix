4b64f6d48735dfd73aba4df53b654dc050a92755
Nick Cao <nickcao@nichi.co>
2025-06-17 21:40:49 -0400

    armchair: persist ssh host keys

--- nixos/armchair/hardware-configuration.nix
+++ nixos/armchair/hardware-configuration.nix
@@ -63,7 +63,13 @@
   };
 
   environment.persistence."/persist" = {
-    files = [ "/etc/machine-id" ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
     directories = [
       "/var"
       "/root"
