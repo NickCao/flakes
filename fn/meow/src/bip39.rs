9a1b21f42993d2798ded2b48e72a2143e802befe
Nick Cao <nickcao@nichi.co>
2025-11-25 19:52:52 -0500

    fn/meow: fix bug causing empty path segment

--- fn/meow/src/bip39.rs
+++ fn/meow/src/bip39.rs
@@ -1,13 +1,13 @@
-use rand::seq::SliceRandom;
+use rand::prelude::IndexedRandom;
 
 lazy_static::lazy_static! {
-    static ref WORDLIST: Vec<&'static str> = std::include_str!("english.txt").split('\n').collect();
+    pub static ref WORDLIST: Vec<&'static str> = std::include_str!("english.txt").split('\n').filter(|s| !s.is_empty()).collect();
 }
 
 /// generate a mnemonic of length `len`, separated by hyphen
 pub fn mnemonic(len: usize) -> String {
     WORDLIST
-        .choose_multiple(&mut rand::thread_rng(), len)
+        .choose_multiple(&mut rand::rng(), len)
         .copied()
         .collect::<Vec<&str>>()
         .join("-")
