0d6c2a35ea9884e35baf06cce48b0517dae61720
Nick Cao <nickcao@nichi.co>
2026-02-09 16:55:19 -0500

    infra/hcloud: drop use of deprecated datacenter attr

--- infra/hcloud.tf
+++ infra/hcloud.tf
@@ -1,36 +1,36 @@
 locals {
   hnodes = {
     iad0 = {
-      datacenter = "ash-dc1"
-      plan       = "cpx11"
-      tags       = ["hetzner", "nameserver"]
+      location = "ash"
+      plan     = "cpx11"
+      tags     = ["hetzner", "nameserver"]
     }
     hio0 = {
-      datacenter = "hil-dc1"
-      plan       = "cpx31"
-      tags       = ["hetzner"]
+      location = "hil"
+      plan     = "cpx31"
+      tags     = ["hetzner"]
     }
     hel0 = {
-      datacenter = "hel1-dc2"
-      plan       = "cx23"
-      tags       = ["hetzner"]
+      location = "hel1"
+      plan     = "cx23"
+      tags     = ["hetzner"]
     }
     hel1 = {
-      datacenter = "hel1-dc2"
-      plan       = "cx23"
-      tags       = ["hetzner"]
+      location = "hel1"
+      plan     = "cx23"
+      tags     = ["hetzner"]
     }
   }
 }
 
 module "hcloud" {
-  source     = "./modules/hcloud"
-  for_each   = local.hnodes
-  hostname   = each.key
-  fqdn       = "${each.key}.nichi.link"
-  datacenter = each.value.datacenter
-  plan       = each.value.plan
-  tags       = each.value.tags
+  source   = "./modules/hcloud"
+  for_each = local.hnodes
+  hostname = each.key
+  fqdn     = "${each.key}.nichi.link"
+  location = each.value.location
+  plan     = each.value.plan
+  tags     = each.value.tags
 }
 
 resource "hcloud_zone" "nichi_link" {
