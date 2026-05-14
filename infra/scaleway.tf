5686a878d1df46e971a88dc77d12157aafaef55e
Nick Cao <nickcao@nichi.co>
2026-05-14 18:43:33 -0400

    infra/scw: add bucket for 3-2-1

--- infra/scaleway.tf
+++ infra/scaleway.tf
@@ -1,3 +1,27 @@
 resource "scaleway_account_project" "storage" {
   name = "storage"
 }
+
+resource "scaleway_object_bucket" "nichi_backup_par" {
+  project_id = scaleway_account_project.storage.id
+
+  name   = "nichi-backup-par"
+  region = "fr-par"
+
+  lifecycle_rule {
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 1
+  }
+}
+
+# FIXME: enable manually
+# https://github.com/scaleway/terraform-provider-scaleway/issues/3985
+# resource "scaleway_object_bucket_server_side_encryption_configuration" "nichi_backup_par" {
+#   bucket = scaleway_object_bucket.nichi_backup_par.name
+#
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
