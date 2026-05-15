131f819398a90976f59ca7cf07b6395db723c94d
Nick Cao <nickcao@nichi.co>
2026-05-14 20:53:12 -0400

    infra/scw: setup bucket mirroring

--- infra/scaleway.tf
+++ infra/scaleway.tf
@@ -14,6 +14,13 @@ resource "scaleway_object_bucket" "nichi_backup_par" {
   }
 }
 
+resource "scaleway_object_bucket_acl" "nichi_backup_par" {
+  project_id = scaleway_account_project.storage.id
+
+  bucket = scaleway_object_bucket.nichi_backup_par.id
+  acl    = "private"
+}
+
 # FIXME: enable manually
 # https://github.com/scaleway/terraform-provider-scaleway/issues/3985
 # resource "scaleway_object_bucket_server_side_encryption_configuration" "nichi_backup_par" {
@@ -25,3 +32,79 @@ resource "scaleway_object_bucket" "nichi_backup_par" {
 #     }
 #   }
 # }
+
+resource "scaleway_iam_application" "rclone" {
+  name = "rclone"
+}
+
+resource "scaleway_iam_policy" "rclone" {
+  name           = "rclone"
+  application_id = scaleway_iam_application.rclone.id
+  rule {
+    project_ids          = [scaleway_account_project.storage.id]
+    permission_set_names = ["ObjectStorageFullAccess"]
+  }
+}
+
+resource "scaleway_iam_api_key" "rclone" {
+  application_id     = scaleway_iam_application.rclone.id
+  default_project_id = scaleway_account_project.storage.id
+}
+
+resource "scaleway_secret" "rclone" {
+  project_id = scaleway_account_project.storage.id
+
+  name   = "rclone"
+  region = scaleway_object_bucket.nichi_backup_par.region
+}
+
+resource "scaleway_secret_version" "rclone_v1" {
+  region    = scaleway_object_bucket.nichi_backup_par.region
+  secret_id = scaleway_secret.rclone.id
+  data = <<EOT
+[b2]
+type = b2
+account = ${local.secrets.b2.account}
+key = ${local.secrets.b2.key}
+hard_delete = true
+
+[scaleway]
+type = s3
+provider = Scaleway
+env_auth = false
+endpoint = s3.fr-par.scw.cloud
+access_key_id = ${scaleway_iam_api_key.rclone.access_key}
+secret_access_key = ${scaleway_iam_api_key.rclone.secret_key}
+region = fr-par
+location_constraint =
+acl = private
+force_path_style = false
+server_side_encryption =
+storage_class = ONEZONE_IA
+EOT
+}
+
+resource "scaleway_job_definition" "rclone" {
+  project_id = scaleway_account_project.storage.id
+
+  name   = "rclone"
+  region = scaleway_object_bucket.nichi_backup_par.region
+
+  timeout                = "30m"
+  cpu_limit              = 1120
+  memory_limit           = 512
+  local_storage_capacity = 1000
+
+  image_uri = "ghcr.io/rclone/rclone:1.74.1"
+  args = [
+    "sync",
+    "--checksum", "--progress",
+    "--transfers", "32",
+    "b2:nichi-backup", "scaleway:nichi-backup-par",
+  ]
+
+  secret_reference {
+    secret_id = scaleway_secret.rclone.id
+    file      = "/config/rclone/rclone.conf"
+  }
+}
