b48d65b58504159a261caeefddcfe3ff16c544f8
Nick Cao <nickcao@nichi.co>
2026-06-01 21:18:56 -0400

    pkgs/caddy-nickcao: 0-unstable-2026-05-12 -> 0-unstable-2026-06-03

--- pkgs/caddy-nickcao/package.nix
+++ pkgs/caddy-nickcao/package.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "caddy";
-  version = "0-unstable-2026-05-12";
+  version = "0-unstable-2026-06-03";
 
   src = fetchFromGitHub {
     owner = "NickCao";
     repo = "caddy";
-    rev = "9c3c685bb2303412d1d10a98c38838a37ceb835f";
-    hash = "sha256-wvhVmi2Qoq0iLo6GOxSf6pdqiAtI6rfCZEec3y/NoJs=";
+    rev = "85dddc0fe6a219c64695fc58027f2b1badb0ad91";
+    hash = "sha256-p++tqd75hL+GZv61upr4s08eoSEON6BJQJFYol3Z2KY=";
   };
 
-  vendorHash = "sha256-wuPEIWmtX0udtiNLKF2DIonmM7mVIfe9vf/j0we5LFE=";
+  vendorHash = "sha256-lAW7LNoyVY0BB4IxdttpXTV0ZVWqgrFql6u85+9WLYs=";
 
   subPackages = [ "cmd/caddy" ];
 
