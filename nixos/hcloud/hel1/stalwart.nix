c770db2c100982f8f8be01f2495e07e9a8611649
Nick Cao <nickcao@nichi.co>
2026-07-10 11:03:28 -0400

    hel1/stalwart: apply oidc config

--- nixos/hcloud/hel1/stalwart.nix
+++ nixos/hcloud/hel1/stalwart.nix
@@ -1,3 +1,58 @@
-{ ... }: {
-  services.stalwart.enable = true;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  sops.secrets.stalwart = { };
+
+  services.stalwart = {
+    enable = true;
+    # credentialFile = pkgs.writeText "stalwart-creds" ''
+    #   STALWART_RECOVERY_ADMIN=admin:f44278e8bad73a35384c726d0416ee37
+    # '';
+    apply = {
+      enable = true;
+      credentialFile = config.sops.secrets.stalwart.path;
+      plan = [
+        {
+          "@type" = "upsert";
+          matchOn = [ "description" ];
+          object = "Directory";
+          value = {
+            directory-iz07tzgaaaqa = {
+              "@type" = "Oidc";
+              description = "keycloak";
+              memberTenantId = null;
+              claimUsername = "preferred_username";
+              claimName = null;
+              claimGroups = null;
+              issuerUrl = "https://id.nichi.co/realms/nichi";
+              usernameDomain = "scp.link";
+              requireAudience = "stalwart";
+              requireScopes = {
+                email = true;
+                openid = true;
+                profile = true;
+                stalwart = true;
+              };
+            };
+          };
+        }
+      ];
+    };
+  };
+
+  cloud.caddy.settings.apps.http.servers.default.routes = lib.singleton {
+    match = lib.singleton { host = lib.singleton "mail.scp.link"; };
+    handle = lib.singleton {
+      handler = "reverse_proxy";
+      transport = {
+        protocol = "http";
+        proxy_protocol = "v2";
+      };
+      upstreams = lib.singleton { dial = "127.0.0.1:8080"; };
+    };
+  };
 }
