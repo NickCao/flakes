504e0ec0223a64d93d0b1b68fd0fad2f5fc4956f
Nick Cao <nickcao@nichi.co>
2026-09-17 19:27:48 -0400

    hel1/journald-upload: enable

--- nixos/hcloud/hel1/victorialogs.nix
+++ nixos/hcloud/hel1/victorialogs.nix
@@ -5,8 +5,6 @@
     listenAddress = "127.0.0.1:9428";
   };
 
-  services.journald.upload.enable = lib.mkForce false; # FIXME
-
   cloud.caddy.mtls = lib.singleton "logs.nichi.co";
   cloud.caddy.settings.apps.http.servers.default.routes = lib.singleton {
     match = lib.singleton {
