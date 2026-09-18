b23853c7b46983b5b902fa264aa0b1dd6bc97229
Nick Cao <nickcao@nichi.co>
2026-09-18 08:45:28 -0400

    subframe/immich: expose metrics

--- nixos/subframe/immich.nix
+++ nixos/subframe/immich.nix
@@ -4,5 +4,10 @@
     host = "";
     port = 2283;
     settings = { };
+    environment = {
+      IMMICH_TELEMETRY_INCLUDE = "all";
+      IMMICH_API_METRICS_PORT = toString 2284;
+      IMMICH_MICROSERVICES_METRICS_PORT = toString 2285;
+    };
   };
 }
