c8a17cb65edf6f1ca03492ead989834640788939
Nick Cao <nickcao@nichi.co>
2026-09-19 19:18:55 -0400

    subframe/immich: disable metrics as they only report performance

--- nixos/subframe/immich.nix
+++ nixos/subframe/immich.nix
@@ -4,10 +4,6 @@
     host = "";
     port = 2283;
     settings = { };
-    environment = {
-      IMMICH_TELEMETRY_INCLUDE = "all";
-      IMMICH_API_METRICS_PORT = toString 2284;
-      IMMICH_MICROSERVICES_METRICS_PORT = toString 2285;
-    };
+    environment = { };
   };
 }
